a8a09e08b8ca1ba85deaea7c07d4202e68bcac5a
Hajar AIT EL KADI <hajar.aitelkadi.o@gmail.com>
2022-08-30 10:33:47 +0200

    fix: change doc count for elastic check

--- task_functions.py
+++ task_functions.py
@@ -926,7 +926,7 @@ def check_elastic_index(**kwargs):
 
     logging.info(f"******************** Documents indexed: {doc_count}")
 
-    if float(count_sieges) - float(doc_count) > 3500:
+    if float(count_sieges) - float(doc_count) > 7000:
         raise ValueError(
             f"*******The data has not been correctly indexed: "
             f"{doc_count} documents indexed instead of {count_sieges}."
