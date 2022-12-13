471f3f3e994b3ab33f8b46edccbd9e0d49ac5995
Hajar AIT EL KADI <hajar.aitelkadi.o@gmail.com>
2022-12-13 17:05:51 +0100

    feat: add finess, uai, elu, colter, rge and spectacle preprocessing tasks to DAG

--- DAG-insert-elk-sirene.py
+++ DAG-insert-elk-sirene.py
@@ -12,19 +12,23 @@ from dag_datalake_sirene.task_functions import (
     check_elastic_index,
     count_nombre_etablissements,
     count_nombre_etablissements_ouverts,
+    create_colter_table,
     create_convention_collective_table,
+    create_elu_table,
+    create_finess_table,
     create_dirig_pm_table,
     create_dirig_pp_table,
     create_elastic_index,
     create_etablissement_table,
+    create_rge_table,
     create_siege_only_table,
     create_sitemap,
     create_sqlite_database,
+    create_uai_table,
     create_unite_legale_table,
     fill_elastic_index,
     get_colors,
     get_object_minio,
-    put_object_minio,
     update_color_file,
     update_sitemap,
 )
@@ -128,6 +132,42 @@ with DAG(
         python_callable=create_convention_collective_table,
     )
 
+    create_rge_table = PythonOperator(
+        task_id="create_rge_table",
+        provide_context=True,
+        python_callable=create_rge_table,
+    )
+
+    create_finess_table = PythonOperator(
+        task_id="create_finess_table",
+        provide_context=True,
+        python_callable=create_finess_table,
+    )
+
+    create_uai_table = PythonOperator(
+        task_id="create_uai_table",
+        provide_context=True,
+        python_callable=create_uai_table,
+    )
+
+    create_spectacle_table = PythonOperator(
+        task_id="create_spectacle_table",
+        provide_context=True,
+        python_callable=create_spectacle_table,
+    )
+
+    create_elu_table = PythonOperator(
+        task_id="create_elu_table",
+        provide_context=True,
+        python_callable=create_elu_table,
+    )
+
+    create_colter_table = PythonOperator(
+        task_id="create_colter_table",
+        provide_context=True,
+        python_callable=create_colter_table,
+    )
+
     create_elastic_index = PythonOperator(
         task_id="create_elastic_index",
         provide_context=True,
@@ -164,191 +204,6 @@ with DAG(
         python_callable=update_color_file,
     )
 
-    get_latest_colter_data = PythonOperator(
-        task_id="get_latest_colter_data",
-        python_callable=get_object_minio,
-        op_args=(
-            "colter-latest.csv",
-            f"ae/data_aggregation/{ENV}/colter/",
-            f"{TMP_FOLDER}{DAG_FOLDER}{DAG_NAME}/data/colter-latest.csv",
-        ),
-    )
-
-    update_es_colter = PythonOperator(
-        task_id="update_es_colter",
-        python_callable=update_elasticsearch_with_new_data,
-        op_args=(
-            "colter",
-            f"{TMP_FOLDER}{DAG_FOLDER}{DAG_NAME}/data/colter-latest.csv",
-            "colter-errors.txt",
-            "next",
-        ),
-    )
-
-    put_file_error_to_minio_colter = PythonOperator(
-        task_id="put_file_error_to_minio_colter",
-        python_callable=put_object_minio,
-        op_args=(
-            "colter-errors.txt",
-            f"ae/data_aggregation/{ENV}/colter/colter-errors.txt",
-            f"{TMP_FOLDER}{DAG_FOLDER}{DAG_NAME}/data/",
-        ),
-    )
-
-    get_latest_elu_data = PythonOperator(
-        task_id="get_latest_elu_data",
-        python_callable=get_object_minio,
-        op_args=(
-            "elu-latest.csv",
-            f"ae/data_aggregation/{ENV}/colter/",
-            f"{TMP_FOLDER}{DAG_FOLDER}{DAG_NAME}/data/elu-latest.csv",
-        ),
-    )
-
-    update_es_elu = PythonOperator(
-        task_id="update_es_elu",
-        python_callable=update_elasticsearch_with_new_data,
-        op_args=(
-            "elu",
-            f"{TMP_FOLDER}{DAG_FOLDER}{DAG_NAME}/data/elu-latest.csv",
-            "elu-errors.txt",
-            "next",
-        ),
-    )
-
-    put_file_error_to_minio_elu = PythonOperator(
-        task_id="put_file_error_to_minio_elu",
-        python_callable=put_object_minio,
-        op_args=(
-            "elu-errors.txt",
-            f"ae/data_aggregation/{ENV}/colter/elu-errors.txt",
-            f"{TMP_FOLDER}{DAG_FOLDER}{DAG_NAME}/data/",
-        ),
-    )
-
-    get_latest_rge_data = PythonOperator(
-        task_id="get_latest_rge_data",
-        python_callable=get_object_minio,
-        op_args=(
-            "rge-latest.csv",
-            f"ae/data_aggregation/{ENV}/rge/",
-            f"{TMP_FOLDER}{DAG_FOLDER}{DAG_NAME}/data/rge-latest.csv",
-        ),
-    )
-
-    update_es_rge = PythonOperator(
-        task_id="update_es_rge",
-        python_callable=update_elasticsearch_with_new_data,
-        op_args=(
-            "rge",
-            f"{TMP_FOLDER}{DAG_FOLDER}{DAG_NAME}/data/rge-latest.csv",
-            "rge-errors.txt",
-            "next",
-        ),
-    )
-
-    put_file_error_to_minio_rge = PythonOperator(
-        task_id="put_file_error_to_minio_rge",
-        python_callable=put_object_minio,
-        op_args=(
-            "rge-errors.txt",
-            f"ae/data_aggregation/{ENV}/rge/rge-errors.txt",
-            f"{TMP_FOLDER}{DAG_FOLDER}{DAG_NAME}/data/",
-        ),
-    )
-
-    get_latest_finess_data = PythonOperator(
-        task_id="get_latest_finess_data",
-        python_callable=get_object_minio,
-        op_args=(
-            "finess-latest.csv",
-            f"ae/data_aggregation/{ENV}/finess/",
-            f"{TMP_FOLDER}{DAG_FOLDER}{DAG_NAME}/data/finess-latest.csv",
-        ),
-    )
-
-    update_es_finess = PythonOperator(
-        task_id="update_es_finess",
-        python_callable=update_elasticsearch_with_new_data,
-        op_args=(
-            "finess",
-            f"{TMP_FOLDER}{DAG_FOLDER}{DAG_NAME}/data/finess-latest.csv",
-            "finess-errors.txt",
-            "next",
-        ),
-    )
-
-    put_file_error_to_minio_finess = PythonOperator(
-        task_id="put_file_error_to_minio_finess",
-        python_callable=put_object_minio,
-        op_args=(
-            "finess-errors.txt",
-            f"ae/data_aggregation/{ENV}/finess/finess-errors.txt",
-            f"{TMP_FOLDER}{DAG_FOLDER}{DAG_NAME}/data/",
-        ),
-    )
-
-    get_latest_spectacle_data = PythonOperator(
-        task_id="get_latest_spectacle_data",
-        python_callable=get_object_minio,
-        op_args=(
-            "spectacle-latest.csv",
-            f"ae/data_aggregation/{ENV}/spectacle/",
-            f"{TMP_FOLDER}{DAG_FOLDER}{DAG_NAME}/data/spectacle-latest.csv",
-        ),
-    )
-
-    update_es_spectacle = PythonOperator(
-        task_id="update_es_spectacle",
-        python_callable=update_elasticsearch_with_new_data,
-        op_args=(
-            "spectacle",
-            f"{TMP_FOLDER}{DAG_FOLDER}{DAG_NAME}/data/spectacle-latest.csv",
-            "spectacle-errors.txt",
-            "next",
-        ),
-    )
-
-    put_file_error_to_minio_spectacle = PythonOperator(
-        task_id="put_file_error_to_minio_spectacle",
-        python_callable=put_object_minio,
-        op_args=(
-            "spectacle-errors.txt",
-            f"ae/data_aggregation/{ENV}/spectacle/spectacle-errors.txt",
-            f"{TMP_FOLDER}{DAG_FOLDER}{DAG_NAME}/data/",
-        ),
-    )
-    get_latest_uai_data = PythonOperator(
-        task_id="get_latest_uai_data",
-        python_callable=get_object_minio,
-        op_args=(
-            "uai-latest.csv",
-            f"ae/data_aggregation/{ENV}/uai/",
-            f"{TMP_FOLDER}{DAG_FOLDER}{DAG_NAME}/data/uai-latest.csv",
-        ),
-    )
-
-    update_es_uai = PythonOperator(
-        task_id="update_es_uai",
-        python_callable=update_elasticsearch_with_new_data,
-        op_args=(
-            "uai",
-            f"{TMP_FOLDER}{DAG_FOLDER}{DAG_NAME}/data/uai-latest.csv",
-            "uai-errors.txt",
-            "next",
-        ),
-    )
-
-    put_file_error_to_minio_uai = PythonOperator(
-        task_id="put_file_error_to_minio_uai",
-        python_callable=put_object_minio,
-        op_args=(
-            "uai-errors.txt",
-            f"ae/data_aggregation/{ENV}/uai/uai-errors.txt",
-            f"{TMP_FOLDER}{DAG_FOLDER}{DAG_NAME}/data/",
-        ),
-    )
-
     execute_aio_container = SSHOperator(
         ssh_conn_id="SERVER",
         task_id="execute_aio_container",
@@ -372,49 +227,34 @@ with DAG(
 
     clean_previous_folder.set_upstream(get_colors)
     create_sqlite_database.set_upstream(clean_previous_folder)
+
     create_unite_legale_table.set_upstream(create_sqlite_database)
+
     create_etablissement_table.set_upstream(create_unite_legale_table)
     count_nombre_etablissements.set_upstream(create_etablissement_table)
     count_nombre_etablissements_ouverts.set_upstream(count_nombre_etablissements)
     create_siege_only_table.set_upstream(count_nombre_etablissements_ouverts)
+
     get_dirigeants_database.set_upstream(create_siege_only_table)
     create_dirig_pp_table.set_upstream(get_dirigeants_database)
     create_dirig_pm_table.set_upstream(create_dirig_pp_table)
+
     create_convention_collective_table.set_upstream(create_dirig_pm_table)
-    create_elastic_index.set_upstream(create_convention_collective_table)
+    create_rge_table.set_upstream(create_convention_collective_table)
+    create_finess_table.set_upstream(create_rge_table)
+    create_uai_table.set_upstream(create_finess_table)
+    create_spectacle_table.set_upstream(create_uai_table)
+    create_elu_table.set_upstream(create_spectacle_table)
+    create_colter_table.set_upstream(create_elu_table)
+
+    create_elastic_index.set_upstream(create_colter_table)
     fill_elastic_index.set_upstream(create_elastic_index)
     check_elastic_index.set_upstream(fill_elastic_index)
+
     create_sitemap.set_upstream(check_elastic_index)
     update_sitemap.set_upstream(create_sitemap)
 
-    get_latest_colter_data.set_upstream(check_elastic_index)
-    update_es_colter.set_upstream(get_latest_colter_data)
-    put_file_error_to_minio_colter.set_upstream(update_es_colter)
-    get_latest_elu_data.set_upstream(put_file_error_to_minio_colter)
-    update_es_elu.set_upstream(get_latest_elu_data)
-    put_file_error_to_minio_elu.set_upstream(update_es_elu)
-
-    get_latest_rge_data.set_upstream(check_elastic_index)
-    update_es_rge.set_upstream(get_latest_rge_data)
-    put_file_error_to_minio_rge.set_upstream(update_es_rge)
-
-    get_latest_finess_data.set_upstream(check_elastic_index)
-    update_es_finess.set_upstream(get_latest_finess_data)
-    put_file_error_to_minio_finess.set_upstream(update_es_finess)
-
-    get_latest_spectacle_data.set_upstream(check_elastic_index)
-    update_es_spectacle.set_upstream(get_latest_spectacle_data)
-    put_file_error_to_minio_spectacle.set_upstream(update_es_spectacle)
-
-    get_latest_uai_data.set_upstream(check_elastic_index)
-    update_es_uai.set_upstream(get_latest_uai_data)
-    put_file_error_to_minio_uai.set_upstream(update_es_uai)
-
-    update_color_file.set_upstream(put_file_error_to_minio_elu)
-    update_color_file.set_upstream(put_file_error_to_minio_rge)
-    update_color_file.set_upstream(put_file_error_to_minio_finess)
-    update_color_file.set_upstream(put_file_error_to_minio_spectacle)
-    update_color_file.set_upstream(put_file_error_to_minio_uai)
+    update_color_file.set_upstream(check_elastic_index)
 
     execute_aio_container.set_upstream(update_color_file)
 
