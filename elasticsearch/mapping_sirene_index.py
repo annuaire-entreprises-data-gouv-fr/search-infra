5f92bbcd5c0efe3b32a5058464ca34c9aa338d34
Hajar AIT EL KADI <hajar.aitelkadi.o@gmail.com>
2022-12-26 11:11:05 +0100

    feat: add etablissement to elasticsearch mapping

--- elasticsearch/mapping_sirene_index.py
+++ elasticsearch/mapping_sirene_index.py
@@ -57,6 +57,8 @@ annuaire_analyzer = analyzer(
     ],
 )
 
+ELASTIC_SHARDS = 4
+
 
 class ElasticsearchDirigeantPPIndex(InnerDoc):
     nom = Text(analyzer=annuaire_analyzer, fields={"keyword": Keyword()})
@@ -72,6 +74,63 @@ class ElasticsearchDirigeantPMIndex(InnerDoc):
     denomination = Text(analyzer=annuaire_analyzer, fields={"keyword": Keyword()})
     sigle = Text(analyzer=annuaire_analyzer)
     qualite = Text(analyzer=annuaire_analyzer)
+
+
+class ElasticsearchEtablissementIndex(InnerDoc):
+    activite_principale = Text()
+    activite_principale_registre_metier = Keyword()
+    adresse = Text(analyzer=annuaire_analyzer)
+    cedex = Keyword()
+    cedex_2 = Text()
+    code_pays_etranger = Text()
+    code_pays_etranger_2 = Text()
+    code_postal = Keyword()
+    commune = Keyword()
+    commune_2 = Text()
+    concat_nom_enseigne_adresse_siren_siret = Text(
+        analyzer=annuaire_analyzer, fields={"keyword": Keyword()}
+    )
+    coordonnees = GeoPoint()
+    complement_adresse = Text()
+    complement_adresse_2 = Text()
+    date_creation = Date()
+    date_debut_activite = Date()
+    departement = Keyword()
+    distribution_speciale = Text()
+    distribution_speciale_2 = Text()
+    enseigne_1 = Text(analyzer=annuaire_analyzer)
+    enseigne_2 = Text(analyzer=annuaire_analyzer)
+    enseigne_3 = Text(analyzer=annuaire_analyzer)
+    est_siege = Boolean()
+    etat_administratif = Keyword()
+    geo_adresse = Text(analyzer=annuaire_analyzer)
+    geo_id = Keyword()
+    id_cc = Text()
+    id_finess = Keyword()
+    id_rge = Text()
+    id_uai = Keyword()
+    indice_repetition = Text()
+    indice_repetition_2 = Text()
+    latitude = Text()
+    libelle_cedex = Text()
+    libelle_cedex_2 = Text()
+    libelle_commune = Text()
+    libelle_commune_2 = Text()
+    libelle_commune_etranger = Text()
+    libelle_commune_etranger_2 = Text()
+    libelle_pays_etranger = Text()
+    libelle_pays_etranger_2 = Text()
+    libelle_voie = Text()
+    libelle_voie_2 = Text()
+    longitude = Text()
+    nom_commercial = Text()
+    numero_voie = Text()
+    numero_voie_2 = Text()
+    siren = Keyword(required=True)
+    siret = Keyword(required=True)
+    tranche_effectif_salarie = Keyword()
+    type_voie = Text()
+    type_voie_2 = Text()
 
 
 class ElasticsearchEluIndex(InnerDoc):
@@ -94,79 +153,42 @@ class ElasticsearchSireneIndex(Document):
 
     """
 
-    activite_principale_siege = Keyword()  # Add index_prefixes option
     activite_principale_unite_legale = Keyword()
-    activite_principale_registre_metier = Keyword()
-    adresse_etablissement = Text(analyzer=annuaire_analyzer)
     categorie_entreprise = Text()
-    cedex = Keyword()
-    code_pays_etranger = Text()
-    code_postal = Keyword()
     colter_code = Keyword()
     colter_code_insee = Keyword()
     colter_elus = Nested(ElasticsearchEluIndex)
     colter_niveau = Keyword()
-    commune = Keyword()
-    complement_adresse = Text()
-    concat_enseigne_adresse = Text(analyzer=annuaire_analyzer)
-    concat_nom_adr_siren = Text(
-        analyzer=annuaire_analyzer, fields={"keyword": Keyword()}
-    )
-    coordonnees = GeoPoint()
-    date_creation_siege = Date()
     date_creation_unite_legale = Date()
-    date_debut_activite_siege = Date()
-    date_mise_a_jour = Date()
-    departement = Keyword()
+    date_mise_a_jour_unite_legale = Date()
     dirigeants_pp = Nested(ElasticsearchDirigeantPPIndex)
     dirigeants_pm = Nested(ElasticsearchDirigeantPMIndex)
-    distribution_speciale = Text()
     economie_sociale_solidaire_unite_legale = Keyword()
-    enseigne = Text(analyzer=annuaire_analyzer)
-    etat_administratif_unite_legale = Keyword()
-    etat_administratif_siege = Keyword()
-    geo_adresse = Text(analyzer=annuaire_analyzer)
-    geo_id = Keyword()
-    identifiant_association_unite_legale = Keyword()
-    indice_repetition = Text()
     est_entrepreneur_individuel = Boolean()
     est_entrepreneur_spectacle = Boolean()
-    est_siege = Boolean()
-    latitude = Text()
-    libelle_cedex = Text()
-    libelle_commune = Text()
-    libelle_commune_etranger = Text()
-    libelle_pays_etranger = Text()
-    libelle_voie = Text()
-    liste_adresses = Text(analyzer=annuaire_analyzer)
-    liste_idcc = Text()
+    etablissements = Nested(ElasticsearchEtablissementIndex)
+    etat_administratif_unite_legale = Keyword()
+    identifiant_association_unite_legale = Keyword()
     liste_dirigeants = Text(analyzer=annuaire_analyzer)
     liste_elus = Text(analyzer=annuaire_analyzer)
-    liste_enseignes = Text(analyzer=annuaire_analyzer)
-    liste_finess = Text()
-    liste_rge = Text()
-    liste_uai = Text()
-    longitude = Text()
     nature_juridique_unite_legale = Integer()
     nom = Text(analyzer=annuaire_analyzer)
     nom_complet = Text(analyzer=annuaire_analyzer, fields={"keyword": Keyword()})
     nom_raison_sociale = Text()
     nombre_etablissements = Integer()  # NaN can't be stored in an integer array
     nombre_etablissements_ouverts = Integer()
-    numero_voie = Text()
     prenom = Text(analyzer=annuaire_analyzer)
     section_activite_principale = Keyword()
     sigle = Keyword()
     siren = Keyword(required=True)
-    siret_siege = Keyword(required=True)
-    type_voie = Text()
-    tranche_effectif_salarie_siege = Keyword()
+    siret_siege = Keyword()
     tranche_effectif_salarie_unite_legale = Keyword()
 
     class Index:
         name = f"siren-{NEXT_COLOR}"
         settings = {
-            "number_of_shards": 1,
+            "number_of_shards": ELASTIC_SHARDS,
             "number_of_replicas": 0,
             "mapping": {"ignore_malformed": True},
+            "index.mapping.nested_objects.limit": 20000,
         }
