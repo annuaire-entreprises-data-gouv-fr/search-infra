daff9628e1ee69ef1c8c576fe78323050b1420f0
Hajar AIT EL KADI <hajar.aitelkadi.o@gmail.com>
2023-01-04 16:24:48 +0100

    feat: add corresponding es fields to complements function

---
+++ helpers/es_fields.py
@@ -0,0 +1,10 @@
+def get_elasticsearch_field_name(param_name: str) -> str:
+    corresponding_es_field = {
+        "liste_idcc": "convention_collective_renseignee",
+        "liste_finess": "est_finess",
+        "liste_rge": "est_rge",
+        "liste_uai": "est_uai",
+    }
+    if param_name in corresponding_es_field:
+        return corresponding_es_field[param_name]
+    return param_name
