5a2e8d0f2a749c8314d1317727aba49268aec77a
Geoffrey Aldebert <geoffrey.aldebert@data.gouv.fr>
2023-03-10 19:24:30 +0100

    feat: add slug in es and refacto sitemap creation

--- sqlite/queries/select_sitemap_fields.py
+++ sqlite/queries/select_sitemap_fields.py
@@ -1,7 +1,10 @@
 select_sitemap_fields_query = """SELECT
-        ul.siren,
         ul.nom_raison_sociale as nom_raison_sociale,
+        ul.denomination_usuelle_1 as denomination_usuelle_1_unite_legale,
+        ul.denomination_usuelle_2 as denomination_usuelle_2_unite_legale,
+        ul.denomination_usuelle_3 as denomination_usuelle_3_unite_legale,
         ul.sigle as sigle,
+        ul.siren as siren,
         ul.etat_administratif_unite_legale as etat_administratif_unite_legale,
         ul.nature_juridique_unite_legale as nature_juridique_unite_legale,
         st.code_postal as code_postal,
