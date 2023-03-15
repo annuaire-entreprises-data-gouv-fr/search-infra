0d325196b02f4717141c206d856ad527660a5288
Geoffrey Aldebert <geoffrey.aldebert@data.gouv.fr>
2023-03-15 11:20:01 +0100

    fix: parse correctly statut_bio

--- elasticsearch/process_unites_legales.py
+++ elasticsearch/process_unites_legales.py
@@ -144,7 +144,7 @@ def process_unites_legales(chunk_unites_legales_sqlite):
 
         # Statut Bio
         if "statut_bio" in unite_legale and json.loads(unite_legale["statut_bio"]):
-            if "valide" in unite_legale["statut_bio"]:
+            if "valide" in json.loads(unite_legale["statut_bio"]):
                 unite_legale_processed["statut_bio"] = "valide"
             else:
                 unite_legale_processed["statut_bio"] = "invalide"
