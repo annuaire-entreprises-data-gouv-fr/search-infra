2046c383e86e8679f1608be6d955d7a379f178a8
Hajar AIT EL KADI <hajar.aitelkadi.o@gmail.com>
2023-04-04 10:20:30 +0200

    fix: keep the latest spectacle statut

--- data_preprocessing/entrepreneur_spectacle.py
+++ data_preprocessing/entrepreneur_spectacle.py
@@ -16,14 +16,29 @@ def preprocess_spectacle_data(data_dir):
         "siren_personne_physique_siret_personne_morale"
     ].str[:9]
     df_spectacle = df_spectacle[
-        ["siren", "est_entrepreneur_spectacle", "statut_du_recepisse"]
+        [
+            "siren",
+            "est_entrepreneur_spectacle",
+            "statut_du_recepisse",
+            "date_de_depot_de_la_declaration_inscrite_sur_le_recepisse",
+        ]
     ]
     df_spectacle = df_spectacle.rename(
-        columns={"statut_du_recepisse": "statut_entrepreneur_spectacle"}
+        columns={
+            "statut_du_recepisse": "statut_entrepreneur_spectacle",
+            "date_de_depot_de_la_declaration_inscrite_sur_le_recepisse": "date",
+        }
     )
     df_spectacle["statut_entrepreneur_spectacle"] = df_spectacle[
         "statut_entrepreneur_spectacle"
     ].apply(lambda x: "valide" if x == "Valide" else "invalide")
     df_spectacle = df_spectacle[df_spectacle["siren"].notna()]
-
-    return df_spectacle
+    df_spectacle["date"] = pd.to_datetime(df_spectacle["date"])
+    # Only keep the latest entry
+    df_spectacle_processed = (
+        df_spectacle.sort_values(by="date", ascending=False)
+        .groupby("siren")
+        .first()
+        .reset_index()
+    )
+    return df_spectacle_processed
