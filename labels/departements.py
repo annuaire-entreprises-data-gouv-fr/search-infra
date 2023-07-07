169d8cf125afb6d187430bbeb6f597c8c8d72e97
Hajar AIT EL KADI <hajar.aitelkadi.o@gmail.com>
2023-07-07 17:28:40 +0200

    fix: add missing dep to etablissements files

--- labels/departements.py
+++ labels/departements.py
@@ -2,7 +2,7 @@
 all_deps = [
     *"-7510".join(list(str(x) for x in range(0, 10))).split("-")[1:],
     *"-751".join(list(str(x) for x in range(9, 21))).split("-")[1:],
-    *["971", "972", "973", "974", "976", "98"],
+    *["971", "972", "973", "974", "975", "976", "977", "978", "98"],
     *"-0".join(list(str(x) for x in range(0, 10))).split("-")[1:],
     *list(str(x) for x in range(10, 20)),
     *["2A", "2B"],
