337b82be49fb287a49fe16f0784416b11268c759
Hajar AIT EL KADI <hajar.aitelkadi.o@gmail.com>
2023-08-09 17:44:11 +0200

    refactor: rename elasticsearch idnex

--- task_functions/create_elastic_index.py
+++ task_functions/create_elastic_index.py
@@ -1,6 +1,6 @@
 import logging
 
-from dag_datalake_sirene.elasticsearch.create_sirene_index import ElasticCreateSiren
+from dag_datalake_sirene.elasticsearch.create_index import ElasticCreateIndex
 from dag_datalake_sirene.task_functions.global_variables import (
     ELASTIC_BULK_SIZE,
     ELASTIC_PASSWORD,
@@ -13,7 +13,7 @@ def create_elastic_index(**kwargs):
     next_color = kwargs["ti"].xcom_pull(key="next_color", task_ids="get_colors")
     elastic_index = f"siren-{next_color}"
     logging.info(f"******************** Index to create: {elastic_index}")
-    create_index = ElasticCreateSiren(
+    create_index = ElasticCreateIndex(
         elastic_url=ELASTIC_URL,
         elastic_index=elastic_index,
         elastic_user=ELASTIC_USER,
