95782fff93b6fb03af87964b0ddc41f8bb8df834
Hajar AIT EL KADI <hajar.aitelkadi.o@gmail.com>
2023-09-01 14:28:17 +0200

    feat: change DAG start date to today

--- DAG-insert-elk-sirene.py
+++ DAG-insert-elk-sirene.py
@@ -93,7 +93,7 @@ with DAG(
     dag_id=DAG_NAME,
     default_args=default_args,
     schedule_interval="0 0 * * 1,3,5",
-    start_date=datetime(2023, 3, 29),
+    start_date=datetime.today(),
     dagrun_timeout=timedelta(minutes=60 * 15),
     tags=["siren"],
 ) as dag:
