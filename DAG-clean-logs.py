f4028fe737277abc0794714bd80e43357619fd61
Hajar AIT EL KADI <hajar.aitelkadi.o@gmail.com>
2023-09-08 09:33:57 +0200

    feat: set dag start date to day before

--- DAG-clean-logs.py
+++ DAG-clean-logs.py
@@ -14,7 +14,7 @@ ENV = Variable.get("ENV")
 default_args = {
     "owner": "airflow",
     "depends_on_past": False,
-    "start_date": datetime(2023, 1, 1),
+    "start_date": datetime.now() - timedelta(days=1),  # Set the start_date to yesterday
     "retries": 1,
     "retry_delay": timedelta(minutes=5),
 }
