888e401eb77b8a6050a0f5ca6974ab877c0269a4
Hajar AIT EL KADI <hajar.aitelkadi.o@gmail.com>
2023-10-25 09:28:06 +0200

    feat: add config file

---
+++ data_pipelines/config.py
@@ -0,0 +1,20 @@
+from airflow.models import Variable
+
+# Global
+AIRFLOW_DAG_HOME = Variable.get("AIRFLOW_DAG_HOME", "/opt/airflow/dags/")
+AIRFLOW_DAG_TMP = Variable.get("AIRFLOW_DAG_TMP", "/tmp/")
+AIRFLOW_ENV = Variable.get("AIRFLOW_ENV", "dev")
+AIRFLOW_URL = Variable.get("AIRFLOW_URL", "")
+
+# Mattermost
+MATTERMOST_DATAGOUV_DATAENG = Variable.get("MATTERMOST_DATAGOUV_DATAENG", "")
+MATTERMOST_DATAGOUV_DATAENG_TEST = Variable.get("MATTERMOST_DATAGOUV_DATAENG_TEST", "")
+
+# Minio
+MINIO_URL = Variable.get("MINIO_URL", "object.files.data.gouv.fr")
+MINIO_BUCKET = Variable.get("MINIO_BUCKET", "")
+SECRET_MINIO_USER = Variable.get("SECRET_MINIO_USER", "")
+SECRET_MINIO_PASSWORD = Variable.get("SECRET_MINIO_PASSWORD", "")
+
+# RNE
+RNE_FTP_URL = Variable.get("RNE_FTP_URL", "")
