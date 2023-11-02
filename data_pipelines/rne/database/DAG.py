d2ff74fc1e42935e98b4a53e021375643ef9d618
Hajar AIT EL KADI <hajar.aitelkadi.o@gmail.com>
2023-11-02 17:04:51 +0100

    feat: process stock files before flux files

--- data_pipelines/rne/database/DAG.py
+++ data_pipelines/rne/database/DAG.py
@@ -14,7 +14,7 @@ from dag_datalake_sirene.data_pipelines.rne.database.task_functions import (
     process_stock_json_files,
     upload_db_to_minio,
     upload_latest_date_rne_minio,
-    notification_mattermost,
+    notification_tchap,
 )
 
 TMP_FOLDER = f"{AIRFLOW_DAG_TMP}rne/"
@@ -24,7 +24,7 @@ with DAG(
     start_date=days_ago(1),
     max_active_runs=1,
     catchup=False,
-    dagrun_timeout=timedelta(minutes=(60 * 8)),
+    dagrun_timeout=timedelta(minutes=(60 * 20)),
     tags=["data_processing", "rne", "dirigeants"],
     params={},
 ) as dag:
@@ -40,11 +40,11 @@ with DAG(
     get_latest_db = PythonOperator(
         task_id="get_latest_db", python_callable=get_latest_db
     )
-    process_flux_json_files = PythonOperator(
-        task_id="process_flux_json_files", python_callable=process_flux_json_files
-    )
     process_stock_json_files = PythonOperator(
         task_id="process_stock_json_files", python_callable=process_stock_json_files
+    )
+    process_flux_json_files = PythonOperator(
+        task_id="process_flux_json_files", python_callable=process_flux_json_files
     )
     upload_db_to_minio = PythonOperator(
         task_id="upload_db_to_minio", python_callable=upload_db_to_minio
@@ -53,15 +53,15 @@ with DAG(
         task_id="upload_latest_date_rne_minio",
         python_callable=upload_latest_date_rne_minio,
     )
-    notification_mattermost = PythonOperator(
-        task_id="notification_mattermost", python_callable=notification_mattermost
+    notification_tchap = PythonOperator(
+        task_id="notification_tchap", python_callable=notification_tchap
     )
 
     get_start_date.set_upstream(clean_previous_outputs)
     create_db.set_upstream(get_start_date)
     get_latest_db.set_upstream(create_db)
-    process_flux_json_files.set_upstream(get_latest_db)
-    process_stock_json_files.set_upstream(process_flux_json_files)
-    upload_db_to_minio.set_upstream(process_stock_json_files)
+    process_stock_json_files.set_upstream(get_latest_db)
+    process_flux_json_files.set_upstream(process_stock_json_files)
+    upload_db_to_minio.set_upstream(process_flux_json_files)
     upload_latest_date_rne_minio.set_upstream(upload_db_to_minio)
-    notification_mattermost.set_upstream(upload_latest_date_rne_minio)
+    notification_tchap.set_upstream(upload_latest_date_rne_minio)
