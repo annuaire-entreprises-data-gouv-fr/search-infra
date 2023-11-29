da11028f8b5876225e0bada3d3fffe0ee1abd085
Hajar AIT EL KADI <hajar.aitelkadi.o@gmail.com>
2023-11-29 17:58:04 +0100

    fix: add f-string

--- elasticsearch/indexing_unite_legale.py
+++ elasticsearch/indexing_unite_legale.py
@@ -42,7 +42,7 @@ def doc_unite_legale_generator(data):
         # as is
         else:
             yield StructureMapping(
-                meta={"id": "document['identifiant']-100"}, **document
+                meta={"id": f"{document['identifiant']}-100"}, **document
             ).to_dict(include_meta=True)
 
 
