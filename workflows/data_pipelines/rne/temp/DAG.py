2f46e6cd49fe0c49d087f5e81006121ab423a359
Hajar AIT EL KADI <hajar.aitelkadi.o@gmail.com>
2024-01-19 14:23:45 +0100

    chore: remove run schedule

--- workflows/data_pipelines/rne/temp/DAG.py
+++ workflows/data_pipelines/rne/temp/DAG.py
@@ -20,7 +20,7 @@ with DAG(
     dag_id="TMP-FIX-RNE-FILES",
     default_args=default_args,
     start_date=datetime(2023, 10, 11),
-    schedule_interval="0 2 * * *",
+    schedule_interval=None,
     max_active_runs=1,
     catchup=False,
     dagrun_timeout=timedelta(minutes=(60 * 20)),
