236c7e144a9db55770e88d891f8af96f9553daf0
Hajar AIT EL KADI <hajar.aitelkadi.o@gmail.com>
2024-02-26 16:15:21 +0100

    feat: add dag to get inclusion data from api

---
+++ workflows/data_pipelines/marche_inclusion/DAG.py
@@ -0,0 +1,56 @@
+from datetime import timedelta
+
+from airflow.models import DAG
+from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
+from airflow.utils.dates import days_ago
+# fmt: off
+from dag_datalake_sirene.workflows.data_pipelines.marche_inclusion.task_functions\
+    import (
+    get_structures_siae,
+    send_file_minio,
+)
+# fmt: on
+from dag_datalake_sirene.config import (
+    AIRFLOW_DAG_TMP,
+    MARCHE_INCLUSION_TMP_FOLDER,
+)
+
+DAG_NAME = "data_processing_marche_inclusion"
+TMP_FOLDER = f"{AIRFLOW_DAG_TMP}marche_inclusion/"
+
+default_args = {
+    "depends_on_past": False,
+    "email_on_failure": True,
+    "email_on_retry": False,
+    "retries": 0,
+}
+
+
+with DAG(
+    dag_id=DAG_NAME,
+    default_args=default_args,
+    schedule_interval="10 0 * * *",  # Run at 12:10 AM every day
+    start_date=days_ago(1),
+    dagrun_timeout=timedelta(minutes=60),
+    tags=["marche inclusion"],
+) as dag:
+    clean_previous_outputs = BashOperator(
+        task_id="clean_previous_outputs",
+        bash_command=(
+            f"rm -rf {MARCHE_INCLUSION_TMP_FOLDER} && "
+            f"mkdir -p {MARCHE_INCLUSION_TMP_FOLDER}"
+        ),
+    )
+
+    get_structures_siae = PythonOperator(
+        task_id="get_structures_siae",
+        python_callable=get_structures_siae,
+    )
+    send_file_minio = PythonOperator(
+        task_id="send_file_minio",
+        python_callable=send_file_minio,
+    )
+
+    get_structures_siae.set_upstream(clean_previous_outputs)
+    send_file_minio.set_upstream(get_structures_siae)
---
+++ workflows/data_pipelines/marche_inclusion/task_functions.py
@@ -0,0 +1,56 @@
+import csv
+import requests
+import logging
+from dag_datalake_sirene.helpers.minio_helpers import minio_client
+from dag_datalake_sirene.config import (
+    MARCHE_INCLUSION_API_URL,
+    MARCHE_INCLUSION_TMP_FOLDER,
+    SECRET_TOKEN_MARCHE_INCLUSION,
+)
+
+
+def call_api_marche_inclusion(number_of_strctures):
+    query_params = f"token={SECRET_TOKEN_MARCHE_INCLUSION}&limit={number_of_strctures}"
+
+    endpoint = f"{MARCHE_INCLUSION_API_URL}{query_params}"
+
+    response = requests.get(endpoint)
+    data = response.json()
+    return data
+
+
+def save_siae_to_csv(data, file_path):
+    csv_data = [
+        [result["siret"], result["kind_parent"]] for result in data.get("results", [])
+    ]
+    with open(file_path, "w", newline="") as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(["siret", "kind_parent"])
+        writer.writerows(csv_data)
+
+
+def get_structures_siae():
+    number_of_structures = 20000
+    response_data = call_api_marche_inclusion(number_of_structures)
+    actual_number_of_structures = response_data.get("count", 0)
+    logging.info(f"Number of structures: {actual_number_of_structures}")
+
+    if actual_number_of_structures > number_of_structures:
+        number_of_structures = actual_number_of_structures
+        response_data = call_api_marche_inclusion(number_of_structures)
+
+    file_path = f"{MARCHE_INCLUSION_TMP_FOLDER}marche_inclusion.csv"
+    save_siae_to_csv(response_data, file_path)
+
+
+def send_file_minio():
+    minio_client.send_files(
+        list_files=[
+            {
+                "source_path": MARCHE_INCLUSION_TMP_FOLDER,
+                "source_name": "marche_inclusion.csv",
+                "dest_path": "marche_inclusion/",
+                "dest_name": "stock_marche_inclusion.csv",
+            },
+        ],
+    )
