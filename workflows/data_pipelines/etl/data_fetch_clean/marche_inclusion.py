95cd5a9fd6e01e82169c40c37a49694983993f20
Hajar AIT EL KADI <hajar.aitelkadi.o@gmail.com>
2024-02-26 16:16:14 +0100

    feat: preprocess marche inclusion data

---
+++ workflows/data_pipelines/etl/data_fetch_clean/marche_inclusion.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from dag_datalake_sirene.helpers.minio_helpers import minio_client
+
+
+def preprocess_marche_inclusion_data(data_dir):
+    minio_client.get_files(
+        list_files=[
+            {
+                "source_path": "marche_inclusion",
+                "source_name": "stock_marche_inclusion.csv",
+                "dest_path": f"{data_dir}",
+                "dest_name": "marche_inclusion.csv",
+            }
+        ],
+    )
+    df_siae = pd.read_csv(
+        f"{data_dir}marche_inclusion.csv",
+        dtype=str,
+    )
+    df_siae["siren"] = df_siae["siret"].str[0:9]
+
+    df_siae_grouped = (
+        df_siae.groupby("siren")["kind_parent"]
+        .agg(lambda x: list(set(x)))
+        .reset_index()
+    )
+    df_siae_grouped.rename(columns={"kind_parent": "type_siae"}, inplace=True)
+
+    df_siae_grouped["est_siae"] = True
+
+    del df_siae
+
+    return df_siae_grouped
