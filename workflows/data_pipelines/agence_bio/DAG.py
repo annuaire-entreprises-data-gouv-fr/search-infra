02d06f549007e4551111a99109c4e8ac5d9dd2fe
Hajar AIT EL KADI <hajar.aitelkadi.o@gmail.com>
2024-03-18 18:02:42 +0100

    feat: add DAG to preprocess agence bio data

---
+++ workflows/data_pipelines/agence_bio/DAG.py
@@ -0,0 +1,52 @@
+from airflow.models import DAG
+from airflow.operators.python import PythonOperator, ShortCircuitOperator
+from airflow.operators.bash import BashOperator
+
+from airflow.utils.dates import days_ago
+from datetime import timedelta
+from dag_datalake_sirene.config import (
+    AGENCE_BIO_TMP_FOLDER,
+)
+from dag_datalake_sirene.workflows.data_pipelines.agence_bio.task_functions import (
+    process_agence_bio,
+    send_file_to_minio,
+    compare_files_minio,
+    send_notification,
+)
+
+with DAG(
+    dag_id="data_processing_agence_bio",
+    schedule_interval="0 4 * * MON",
+    start_date=days_ago(8),
+    dagrun_timeout=timedelta(minutes=60),
+    tags=["agence bio", "bio", "certifications"],
+    params={},
+    catchup=False,
+) as dag:
+    clean_previous_outputs = BashOperator(
+        task_id="clean_previous_outputs",
+        bash_command=(
+            f"rm -rf {AGENCE_BIO_TMP_FOLDER} && mkdir -p {AGENCE_BIO_TMP_FOLDER}",
+        ),
+    )
+
+    process_agence_bio = PythonOperator(
+        task_id="process_agence_bio", python_callable=process_agence_bio
+    )
+
+    send_file_to_minio = PythonOperator(
+        task_id="send_file_to_minio", python_callable=send_file_to_minio
+    )
+
+    compare_files_minio = ShortCircuitOperator(
+        task_id="compare_files_minio", python_callable=compare_files_minio
+    )
+
+    send_notification = PythonOperator(
+        task_id="send_notification", python_callable=send_notification
+    )
+
+    process_agence_bio.set_upstream(clean_previous_outputs)
+    send_file_to_minio.set_upstream(process_agence_bio)
+    compare_files_minio.set_upstream(send_file_to_minio)
+    send_notification.set_upstream(compare_files_minio)
---
+++ workflows/data_pipelines/agence_bio/README.md
@@ -0,0 +1,11 @@
+# Documentation
+
+## data_processing_agence_bio
+
+| Information | Valeur |
+| -------- | -------- |
+| Fichier source     | `DAG.py`     |
+| Description | Ce traitement permet de constituer un stock sur les entreprises certifiées bio en France. | Hebdomadaire |
+| Données sources | [API Professionnels Bio](https://api.gouv.fr/les-api/api-professionnels-bio) |
+| Données de sorties | Minio |
+| Channel Mattermost d'information | ~annuaire-entrperises-tech Tchap |
