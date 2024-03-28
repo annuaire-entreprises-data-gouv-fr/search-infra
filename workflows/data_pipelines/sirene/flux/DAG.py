79f7aa4ef6ffd7413cce379a85e4daad8dd2f163
Hajar AIT EL KADI <hajar.aitelkadi.o@gmail.com>
2024-03-28 17:27:32 +0100

    chore: increase sirene flux dag timeout to 5 hours

--- workflows/data_pipelines/sirene/flux/DAG.py
+++ workflows/data_pipelines/sirene/flux/DAG.py
@@ -39,7 +39,7 @@ with DAG(
     default_args=default_args,
     schedule_interval="0 4 * * *",  # Daily at 4 AM
     start_date=days_ago(1),
-    dagrun_timeout=timedelta(minutes=60),
+    dagrun_timeout=timedelta(minutes=60 * 5),
     tags=["sirene", "flux"],
 ) as dag:
     clean_previous_outputs = BashOperator(
