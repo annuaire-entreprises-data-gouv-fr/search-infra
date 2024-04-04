d542f514e774e79571f0b9607318e1a32f0a69b8
Hajar AIT EL KADI <hajar.aitelkadi.o@gmail.com>
2024-04-04 15:55:18 +0200

    fix: reverse coordinate fields correspondance

--- workflows/data_pipelines/etl/data_fetch_clean/etablissements.py
+++ workflows/data_pipelines/etl/data_fetch_clean/etablissements.py
@@ -218,8 +218,8 @@ def preprocess_etablissement_data(siret_file_type, data_dir=None):
                 "codePaysEtranger2Etablissement": "code_pays_etranger_2",
                 "libellePaysEtranger2Etablissement": "libelle_pays_etranger_2",
                 "statutDiffusionEtablissement": "statut_diffusion_etablissement",
-                "coordonneeLambertAbscisseEtablissement": "longitude",
-                "coordonneeLambertOrdonneeEtablissement": "latitude",
+                "coordonneeLambertAbscisseEtablissement": "latitude",
+                "coordonneeLambertOrdonneeEtablissement": "longitude",
             }
         )
         yield df_etablissement
