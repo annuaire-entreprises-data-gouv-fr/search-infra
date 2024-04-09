450f75927fe91adcebc545b89a9fc79166aa369f
HAEKADI <hajar.aitelkadi@proton.me>
2024-04-09 15:32:28 +0200

    feat: zip rne flux json file

--- workflows/data_pipelines/rne/flux/flux_tasks.py
+++ workflows/data_pipelines/rne/flux/flux_tasks.py
@@ -46,16 +46,16 @@ def get_latest_json_file(ti):
         f"rne_flux_{start_date}.json.gz",
         f"{last_json_file_path}.gz",
     )
+    logging.info(f"Got zip file : rne_flux_{start_date}.json.gz")
 
     # Unzip json file
     with gzip.open(f"{last_json_file_path}.gz", "rb") as f_in:
         with open(last_json_file_path, "wb") as f_out:
             shutil.copyfileobj(f_in, f_out)
 
-    logging.info(f"***** Got file: {last_json_file_path}")
     ti.xcom_push(key="last_json_file_path", value=last_json_file_path)
-    
-    #os.remove(f"{last_json_file_path}.gz")
+
+    os.remove(f"{last_json_file_path}.gz")
     return last_json_file_path
 
 
@@ -160,13 +160,17 @@ def get_and_save_daily_flux_rne(
             except Exception as e:
                 # If exception accures, save uncompleted file
                 if os.path.exists(json_file_path):
+                    # Zip file
+                    with open(json_file_path, "rb") as f_in:
+                        with gzip.open(f"{json_file_path}.gz", "wb") as f_out:
+                            shutil.copyfileobj(f_in, f_out)
                     minio_client.send_files(
                         list_files=[
                             {
                                 "source_path": f"{RNE_FLUX_DATADIR}/",
-                                "source_name": f"{json_file_name}",
+                                "source_name": f"{json_file_name}.gz",
                                 "dest_path": RNE_MINIO_FLUX_DATA_PATH,
-                                "dest_name": f"{json_file_name}",
+                                "dest_name": f"{json_file_name}.gz",
                             },
                         ],
                     )
@@ -174,6 +178,7 @@ def get_and_save_daily_flux_rne(
                 # JSON file and break the loop
                 logging.info(f"****** Deleting file: {json_file_path}")
                 os.remove(json_file_path)
+                os.remove(f"{json_file_path}.gz")
                 raise Exception(f"Error occurred during the API request: {e}")
 
     if os.path.exists(json_file_path):
