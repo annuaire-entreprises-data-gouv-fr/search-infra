0b69bbbed7fc9e5ad3fd2690717a1e0b4f3fa95e
HAEKADI <hajar.aitelkadi@proton.me>
2024-05-10 12:59:35 +0200

    chore: update ess france source in main etl

--- workflows/data_pipelines/etl/data_fetch_clean/ess_france.py
+++ workflows/data_pipelines/etl/data_fetch_clean/ess_france.py
@@ -1,11 +1,8 @@
 import pandas as pd
-from dag_datalake_sirene.config import URL_ESS_FRANCE
+from dag_datalake_sirene.config import URL_MINIO_ESS_FRANCE
 
 
 def preprocess_ess_france_data(data_dir):
-    df_ess = pd.read_csv(URL_ESS_FRANCE, dtype=str)
-    df_ess["SIREN"] = df_ess["SIREN"].str.zfill(9)
-    df_ess.rename(columns={"SIREN": "siren"}, inplace=True)
-    df_ess["est_ess_france"] = True
-    df_ess = df_ess[["siren", "est_ess_france"]]
+    df_ess = pd.read_csv(URL_MINIO_ESS_FRANCE, dtype=str)
+
     return df_ess
