82952efa204ca801ee64c6d08269547db5f21745
HAEKADI <hajar.aitelkadi@proton.me>
2024-05-14 22:30:05 +0200

    fix: cast bool columns in dataframes

--- workflows/data_pipelines/etl/data_fetch_clean/egapro.py
+++ workflows/data_pipelines/etl/data_fetch_clean/egapro.py
@@ -4,5 +4,6 @@ from dag_datalake_sirene.config import URL_MINIO_EGAPRO
 
 def preprocess_egapro_data(data_dir):
     df_egapro = pd.read_csv(URL_MINIO_EGAPRO, dtype=str)
+    df_egapro["egapro_renseignee"] = df_egapro["egapro_renseignee"].astype(bool)
 
     return df_egapro
