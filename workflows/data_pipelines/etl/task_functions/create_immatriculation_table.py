239c05856fef383301db9449f2163ca716147580
HAEKADI <hajar.aitelkadi@proton.me>
2024-06-09 22:32:26 +0200

    feat: add immatricumlation table to sirene database

---
+++ workflows/data_pipelines/etl/task_functions/create_immatriculation_table.py
@@ -0,0 +1,37 @@
+from dag_datalake_sirene.config import (
+    SIRENE_DATABASE_LOCATION,
+    RNE_DATABASE_LOCATION,
+)
+from dag_datalake_sirene.helpers.sqlite_client import SqliteClient
+
+
+def create_immatriculation_table():
+    # Connect to the destination database
+    sqlite_client_siren = SqliteClient(SIRENE_DATABASE_LOCATION)
+
+    # Attach the RNE database
+    sqlite_client_siren.connect_to_another_db(RNE_DATABASE_LOCATION, "db_rne")
+
+    table_name = "immatriculation"
+
+    # Create table with the same structure as the source table immatriculation
+    sqlite_client_siren.execute(
+        f"""
+    CREATE TABLE IF NOT EXISTS {table_name} AS
+    SELECT * FROM db_rne.{table_name} WHERE 1=0
+    """
+    )
+
+    # Insert distinct rows directly
+    sqlite_client_siren.execute(
+        f"""
+    INSERT INTO {table_name}
+    SELECT DISTINCT * FROM db_rne.{table_name}
+    """
+    )
+
+    # Detach the source database
+    sqlite_client_siren.execute("DETACH DATABASE db_rne")
+
+    # Commit changes and close the connection
+    sqlite_client_siren.commit_and_close_conn()
