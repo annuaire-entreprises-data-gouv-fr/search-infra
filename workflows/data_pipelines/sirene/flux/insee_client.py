68516a63c3f76703fd8aa3ae50322fb2999558b0
HAEKADI <hajar.aitelkadi@proton.me>
2024-07-29 11:22:37 +0200

    feat: use class method in insee client

--- workflows/data_pipelines/sirene/flux/insee_client.py
+++ workflows/data_pipelines/sirene/flux/insee_client.py
@@ -1,40 +1,30 @@
-import logging
-import time
 from typing import Any
 from dag_datalake_sirene.helpers.api_client import APIClient
 
 
-class INSEEAPIClient:
+class INSEEAPIClient(APIClient):
     def __init__(self, api_endpoint: str, bearer_token: str):
-        self.api_client = APIClient(
-            api_endpoint, {"Authorization": f"Bearer {bearer_token}"}
+        super().__init__(
+            base_url=api_endpoint, headers={"Authorization": f"Bearer {bearer_token}"}
         )
 
-    def call_insee_api(self, endpoint, data_property: str) -> list[dict[str, Any]]:
-        cursor: str | None = "*"
-        api_data: list[dict[str, Any]] = []
-        request_count = 0
-
-        while cursor is not None:
-            request_count += 1000
-            if request_count % 10000 == 0:
-                logging.info(f"Request count: {request_count}")
-
-            response = self.api_client.get(endpoint, {"curseur": str(cursor)})
-            response_json = response.json()
-
-            header = response_json.get("header", {})
+    def call_insee_api(self, endpoint: str, data_property: str) -> list[dict[str, Any]]:
+        def next_cursor(response: dict[str, Any]) -> str | None:
+            header = response.get("header", {})
             next_cursor = header.get("curseurSuivant")
             current_cursor = header.get("curseur")
-
-            cursor = (
+            return (
                 None
                 if not next_cursor or next_cursor == current_cursor
                 else next_cursor
             )
-            api_data.extend(response_json.get(data_property, []))
 
-            # Wait for 2 seconds after each call to avoid 429
-            time.sleep(2)
-
-        return api_data
+        return self.fetch_all(
+            endpoint=endpoint,
+            params={},
+            cursor_param="curseur",
+            data_property=data_property,
+            next_cursor_func=next_cursor,
+            batch_size=1000,
+            sleep_time=2.0,
+        )
