bc99a661f367dbf9d383d803d0b84b18224bb8c4
HAEKADI <hajar.aitelkadi@proton.me>
2024-07-29 17:00:38 +0200

    style: rename method

--- workflows/data_pipelines/agence_bio/bio_client.py
+++ workflows/data_pipelines/agence_bio/bio_client.py
@@ -7,7 +7,7 @@ class BIOAPIClient(APIClient):
     def __init__(self):
         super().__init__(base_url="https://opendata.agencebio.org/api/gouv/operateurs/")
 
-    def fetch_all_operators(self) -> list[dict[str, Any]]:
+    def fetch_all_departements(self) -> list[dict[str, Any]]:
         departments = (
             "01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,21,22,"
             "23,24,25,26,27,28,29,2A,2B,30,31,32,33,34,35,36,37,38,39,40,"
