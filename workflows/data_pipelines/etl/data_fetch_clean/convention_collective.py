51c4f66d19b2cc67ab916aec77802fa84c553e88
HAEKADI <hajar.aitelkadi@proton.me>
2024-09-20 20:01:33 +0200

    feat: add last modified date to convention collective

--- workflows/data_pipelines/etl/data_fetch_clean/convention_collective.py
+++ workflows/data_pipelines/etl/data_fetch_clean/convention_collective.py
@@ -1,10 +1,16 @@
 import pandas as pd
 from dag_datalake_sirene.config import URL_MINIO_CONVENTION_COLLECTIVE
+from dag_datalake_sirene.helpers.utils import get_last_modified
 
 
-def preprocess_convcollective_data(data_dir):
+def preprocess_convcollective_data(data_dir, **kwargs):
     df_cc = pd.read_csv(
         URL_MINIO_CONVENTION_COLLECTIVE,
         dtype=str,
+    )
+    # Get the last modified date of the CSV file
+    last_modified = get_last_modified(URL_MINIO_CONVENTION_COLLECTIVE)
+    kwargs["ti"].xcom_push(
+        key="convention_collective_last_modified", value=last_modified
     )
     return df_cc
