ae20013607624d30c44910480987de2e8a322f94
HAEKADI <hajar.aitelkadi@proton.me>
2024-09-20 20:01:55 +0200

    feat: add last modified date to organisme formation

--- workflows/data_pipelines/etl/data_fetch_clean/organisme_formation.py
+++ workflows/data_pipelines/etl/data_fetch_clean/organisme_formation.py
@@ -1,8 +1,9 @@
 import pandas as pd
 from dag_datalake_sirene.config import URL_MINIO_ORGANISME_FORMATION
+from dag_datalake_sirene.helpers.utils import get_last_modified
 
 
-def preprocess_organisme_formation_data(data_dir):
+def preprocess_organisme_formation_data(data_dir, **kwargs):
     df_organisme_formation = pd.read_csv(
         URL_MINIO_ORGANISME_FORMATION,
         dtype={
@@ -11,4 +12,7 @@ def preprocess_organisme_formation_data(data_dir):
             "est_qualiopi": "bool",
         },
     )
+    # Get the last modified date of the CSV file
+    last_modified = get_last_modified(URL_MINIO_ORGANISME_FORMATION)
+    kwargs["ti"].xcom_push(key="organisme_formation_last_modified", value=last_modified)
     return df_organisme_formation
