a18a8f1481b35a32e178748a454c098a735fce35
HAEKADI <hajar.aitelkadi@proton.me>
2024-09-20 20:00:24 +0200

    feat: add last modified date to bilan financier

--- workflows/data_pipelines/etl/data_fetch_clean/bilan_financier.py
+++ workflows/data_pipelines/etl/data_fetch_clean/bilan_financier.py
@@ -1,10 +1,15 @@
 import pandas as pd
+from dag_datalake_sirene.helpers.utils import get_last_modified
 from dag_datalake_sirene.config import URL_BILANS_FINANCIERS
 
 
-def preprocess_bilan_financier_data(data_dir):
+def preprocess_bilan_financier_data(data_dir, **kwargs):
     df_bilan = pd.read_csv(URL_BILANS_FINANCIERS, dtype=str)
     df_bilan["ca"] = df_bilan["ca"].astype(float)
     df_bilan["resultat_net"] = df_bilan["resultat_net"].astype(float)
+
+    # Get the last modified date of the CSV file
+    last_modified = get_last_modified(URL_BILANS_FINANCIERS)
+    kwargs["ti"].xcom_push(key="bilan_financier_last_modified", value=last_modified)
 
     return df_bilan
