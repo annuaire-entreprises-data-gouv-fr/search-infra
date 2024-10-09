c6967276b263c5b49c096de03b6540148209f245
HAEKADI <hajar.aitelkadi@proton.me>
2024-10-09 17:10:40 +0200

    chore: update logging

--- workflows/data_pipelines/rne/flux/rne_api.py
+++ workflows/data_pipelines/rne/flux/rne_api.py
@@ -102,11 +102,11 @@ class ApiRNEClient:
                 elif hasattr(e, "response") and e.response.status_code == 500:
                     if "Allowed memory size of" in str(e.response.content):
                         url = url.replace("pageSize=100", "pageSize=1")
-                        logging.info(f"***Memory Error changing page size to 1 : {e}")
+                        logging.info(f"***Memory Error changing page size to 1 : {url}")
                     else:
                         logging.info(f"***Error HTTP: {e}")
                         url = url.replace("pageSize=100", "pageSize=1")
-                        logging.info(f"***Changing page size to 1: {e}")
+                        logging.info(f"***Changing page size to 1: {url}")
                         time.sleep(60)
                 else:
                     logging.error(f"Error occurred while making API request: {e}")
