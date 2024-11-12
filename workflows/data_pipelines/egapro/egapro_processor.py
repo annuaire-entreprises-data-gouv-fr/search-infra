0a2fbfa6a1ac82fa0b9246affe5a94f2d1ad9b04
HAEKADI <hajar.aitelkadi@proton.me>
2024-11-12 16:06:46 +0100

    chore: log egapro count

--- workflows/data_pipelines/egapro/egapro_processor.py
+++ workflows/data_pipelines/egapro/egapro_processor.py
@@ -1,11 +1,12 @@
 import pandas as pd
+import logging
 from dag_datalake_sirene.helpers.data_processor import DataProcessor
-from dag_datalake_sirene.config.data_sources import EGAPRO
+from dag_datalake_sirene.workflows.data_pipelines.egapro.config import EGAPRO_CONFIG
 
 
 class EgaproProcessor(DataProcessor):
     def __init__(self):
-        super().__init__(EGAPRO)
+        super().__init__(EGAPRO_CONFIG)
 
     def preprocess_data(self, ti):
         df_egapro = pd.read_excel(
@@ -17,8 +18,13 @@ class EgaproProcessor(DataProcessor):
         df_egapro = df_egapro[["SIREN"]]
         df_egapro["egapro_renseignee"] = True
         df_egapro = df_egapro.rename(columns={"SIREN": "siren"})
-        df_egapro.to_csv(f"{self.config.tmp_folder}egapro.csv", index=False)
-        ti.xcom_push(key="nb_siren_egapro", value=str(df_egapro["siren"].nunique()))
+        df_egapro.to_csv(f"{self.config.tmp_folder}/egapro.csv", index=False)
+
+        unique_count = df_egapro["siren"].nunique()
+        ti.xcom_push(key="nb_siren_egapro", value=str(unique_count))
+
+        logging.info(f"Processed {unique_count} unique SIREN values.")
+
         del df_egapro
 
     def send_notification(self, ti):
