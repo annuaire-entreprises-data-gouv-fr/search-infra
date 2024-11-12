c8862ee71d92c0dda67f116182f8abb7c29d213e
HAEKADI <hajar.aitelkadi@proton.me>
2024-11-12 16:06:24 +0100

    chore: update egapro resource id

--- workflows/data_pipelines/egapro/config.py
+++ workflows/data_pipelines/egapro/config.py
@@ -11,8 +11,8 @@ EGAPRO_CONFIG = DataSourceConfig(
     tmp_folder=f"{BASE_TMP_FOLDER}/egapro",
     minio_path="egapro",
     file_name="egapro",
-    resource_id="5b191d0f-b933-40d1-a1a4-9743c9727059",
-    url=f"{DATA_GOUV_BASE_URL}5b191d0f-b933-40d1-a1a4-9743c9727059",
+    resource_id="d434859f-8d3b-4381-bcdb-ec9200653ae6",
+    url=f"{DATA_GOUV_BASE_URL}d434859f-8d3b-4381-bcdb-ec9200653ae6",
     url_minio=f"{MINIO_BASE_URL}egapro/latest/egapro.csv",
     url_minio_metadata=f"{MINIO_BASE_URL}egapro/latest/metadata.json",
 )
