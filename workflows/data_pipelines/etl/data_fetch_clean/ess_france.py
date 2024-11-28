fe1618445f3da643ab4996b9086300d5c2222b6c
HAEKADI <hajar.aitelkadi@proton.me>
2024-11-28 11:09:21 +0100

    chore(ess france): update ess config usage

--- workflows/data_pipelines/etl/data_fetch_clean/ess_france.py
+++ workflows/data_pipelines/etl/data_fetch_clean/ess_france.py
@@ -1,10 +1,12 @@
 import pandas as pd
-from dag_datalake_sirene.config import URL_MINIO_ESS_FRANCE
+from dag_datalake_sirene.workflows.data_pipelines.ess_france.config import (
+    ESS_CONFIG,
+)
 
 
 def preprocess_ess_france_data(data_dir):
     df_ess = pd.read_csv(
-        URL_MINIO_ESS_FRANCE,
+        ESS_CONFIG.url_minio,
         dtype={
             "siren": "object",
             "est_ess_france": "bool",
--- workflows/data_pipelines/etl/task_functions/create_json_last_modified.py
+++ workflows/data_pipelines/etl/task_functions/create_json_last_modified.py
@@ -6,7 +6,6 @@ import requests
 from dag_datalake_sirene.config import (
     AIRFLOW_ETL_DATA_DIR,
     MINIO_DATA_SOURCE_UPDATE_DATES_FILE,
-    URL_MINIO_ESS_FRANCE_METADATA,
     URL_MINIO_RGE_METADATA,
     URL_MINIO_UAI_METADATA,
     URL_MINIO_COLTER_METADATA,
@@ -19,6 +18,9 @@ from dag_datalake_sirene.config import (
 )
 from dag_datalake_sirene.workflows.data_pipelines.egapro.config import EGAPRO_CONFIG
 from dag_datalake_sirene.workflows.data_pipelines.finess.config import FINESS_CONFIG
+from dag_datalake_sirene.workflows.data_pipelines.ess_france.config import (
+    ESS_CONFIG,
+)
 from dag_datalake_sirene.helpers.minio_helpers import minio_client
 from dag_datalake_sirene.helpers.utils import simplify_date
 
@@ -27,7 +29,7 @@ def create_data_source_last_modified_file(**kwargs):
     metadata_dict = {}
 
     metadata_url_to_datasource = {
-        URL_MINIO_ESS_FRANCE_METADATA: "ess_france",
+        ESS_CONFIG.url_minio_metadata: "ess_france",
         URL_MINIO_RGE_METADATA: "rge",
         URL_MINIO_UAI_METADATA: "uai",
         URL_MINIO_COLTER_METADATA: "collectivite_territoriale",
