f5bb0135b4753f6b2092b45f4b4719a03c3ce326
HAEKADI <hajar.aitelkadi@proton.me>
2024-12-04 17:19:57 +0100

    feat(rge): add processor object

---
+++ workflows/data_pipelines/rge/rge_processor.py
@@ -0,0 +1,73 @@
+import pandas as pd
+import requests
+import logging
+import os
+from dag_datalake_sirene.helpers import Notification, DataProcessor
+from dag_datalake_sirene.workflows.data_pipelines.rge.config import RGE_CONFIG
+from airflow.operators.python import get_current_context
+from dag_datalake_sirene.helpers.utils import get_date_last_modified, save_to_metadata
+
+
+class RGEProcessor(DataProcessor):
+    def __init__(self):
+        super().__init__(RGE_CONFIG)
+
+    def download_data(self):
+        list_rge = []
+        try:
+            r = requests.get(self.config.url)
+            r.raise_for_status()
+            data = r.json()
+            list_rge.extend(data["results"])
+
+            while "next" in data:
+                r = requests.get(data["next"])
+                r.raise_for_status()
+                data = r.json()
+                list_rge.extend(data["results"])
+                logging.info("Fetched additional page data.")
+
+            logging.info(
+                f"Data downloaded successfully from {self.config.url}."
+                "Total records: {len(list_rge)}."
+            )
+            return list_rge
+
+        except requests.exceptions.RequestException as e:
+            logging.error(f"Error downloading data from {self.config.url}: {e}")
+            raise e
+
+    def preprocess_data(self):
+        list_rge = self.download_data()
+
+        df_rge = pd.DataFrame(list_rge)
+        df_rge = df_rge[df_rge["siret"].notna()]
+        df_list_rge = (
+            df_rge.groupby(["siret"])["code_qualification"]
+            .apply(list)
+            .reset_index(name="liste_rge")
+        )
+        df_list_rge = df_list_rge[["siret", "liste_rge"]]
+        df_list_rge["liste_rge"] = df_list_rge["liste_rge"].astype(str)
+
+        df_list_rge.to_csv(f"{self.config.tmp_folder}/rge.csv", index=False)
+        DataProcessor._push_unique_count(df_list_rge["siret"], "nb_siret_rge")
+
+        del df_rge
+        del df_list_rge
+
+    def save_date_last_modified(self):
+        date_last_modified = get_date_last_modified(url=RGE_CONFIG.url)
+        metadata_path = os.path.join(f"{self.config.tmp_folder}", "metadata.json")
+
+        # Save the 'last_modified' date to the metadata file
+        save_to_metadata(metadata_path, "last_modified", date_last_modified)
+
+        logging.info(f"Last modified date saved successfully to {metadata_path}")
+
+    def send_file_to_minio(self):
+        super().send_file_to_minio()
+        ti = get_current_context()["ti"]
+        nb_siret = ti.xcom_pull(key="nb_siret_rge", task_ids="preprocess_rge")
+        message = f"{nb_siret} établissements"
+        ti.xcom_push(key=Notification.notification_xcom_key, value=message)
