8bf3da2b23d757a6801d89628f1b1e817d22c685
HAEKADI <hajar.aitelkadi@proton.me>
2025-01-03 13:37:15 +0100

    refactor(flux sirene): update dag

--- workflows/data_pipelines/sirene/flux/DAG.py
+++ workflows/data_pipelines/sirene/flux/DAG.py
@@ -1,116 +1,71 @@
 from datetime import timedelta
 
-from airflow.models import DAG
-from airflow.operators.python import PythonOperator, ShortCircuitOperator
-from airflow.operators.bash import BashOperator
 from airflow.utils.dates import days_ago
 
-from dag_datalake_sirene.workflows.data_pipelines.sirene.flux.task_functions import (
-    get_current_flux_etablissement,
-    get_current_flux_non_diffusible,
-    get_current_flux_unite_legale,
-    send_notification,
-    send_notification_stock,
-    get_stock_non_diffusible,
-    save_date_last_modified,
-    send_flux_minio,
-    send_stock_minio,
-    send_notification_failure_tchap,
+from airflow.decorators import dag, task
+from dag_datalake_sirene.config import EMAIL_LIST
+from dag_datalake_sirene.helpers import Notification
+from dag_datalake_sirene.workflows.data_pipelines.sirene.flux.insee_processor import (
+    InseeFluxProcessor,
 )
 
-from dag_datalake_sirene.helpers.utils import (
-    check_if_monday,
-)
-from dag_datalake_sirene.config import (
-    EMAIL_LIST,
-    INSEE_FLUX_TMP_FOLDER,
+from dag_datalake_sirene.workflows.data_pipelines.sirene.flux.config import (
+    FLUX_SIRENE_CONFIG,
 )
 
-DAG_NAME = "data_processing_sirene_flux"
 
 default_args = {
     "depends_on_past": False,
     "email_on_failure": True,
     "email_on_retry": False,
-    "retries": 1,
-    "retry_delay": timedelta(minutes=5),
     "email": EMAIL_LIST,
+    "retries": 1,
 }
 
 
-with DAG(
-    dag_id=DAG_NAME,
+@dag(
+    tags=["sirene", "flux"],
     default_args=default_args,
     schedule_interval="0 4 2-31 * *",  # Daily at 4 AM except the 1st of every month
     start_date=days_ago(1),
     dagrun_timeout=timedelta(minutes=60 * 5),
-    tags=["sirene", "flux"],
+    params={},
     catchup=False,
-    on_failure_callback=send_notification_failure_tchap,
     max_active_runs=1,
-) as dag:
-    clean_previous_outputs = BashOperator(
-        task_id="clean_previous_outputs",
-        bash_command=(
-            f"rm -rf {INSEE_FLUX_TMP_FOLDER} && mkdir -p {INSEE_FLUX_TMP_FOLDER}"
-        ),
+    on_failure_callback=Notification.send_notification_tchap,
+    on_success_callback=Notification.send_notification_tchap,
+)
+def data_processing_sirene_flux():
+    insee_processor = InseeFluxProcessor()
+
+    @task.bash
+    def clean_previous_outputs():
+        return f"rm -rf {FLUX_SIRENE_CONFIG.tmp_folder} && mkdir -p {FLUX_SIRENE_CONFIG.tmp_folder}"
+
+    @task
+    def get_flux_unites_legales():
+        return insee_processor.get_current_flux_unite_legale()
+
+    @task
+    def get_flux_etablissements():
+        return insee_processor.get_current_flux_etablissement()
+
+    @task
+    def save_date_last_modified():
+        return insee_processor.save_date_last_modified()
+
+    @task
+    def send_flux_to_minio():
+        return insee_processor.send_flux_to_minio()
+
+    (
+        clean_previous_outputs()
+        >> get_flux_unites_legales()
+        >> get_flux_etablissements()
+        >> save_date_last_modified()
+        >> send_flux_to_minio()
     )
 
-    get_current_flux_unite_legale = PythonOperator(
-        task_id="get_current_flux_unite_legale",
-        python_callable=get_current_flux_unite_legale,
-    )
 
-    get_current_flux_etablissement = PythonOperator(
-        task_id="get_current_flux_etablissement",
-        python_callable=get_current_flux_etablissement,
-    )
-
-    get_current_flux_non_diffusible = PythonOperator(
-        task_id="get_current_flux_non_diffusible",
-        python_callable=get_current_flux_non_diffusible,
-    )
-    save_date_last_modified = PythonOperator(
-        task_id="save_date_last_modified",
-        python_callable=save_date_last_modified,
-    )
-
-    send_flux_minio = PythonOperator(
-        task_id="send_flux_minio",
-        python_callable=send_flux_minio,
-    )
-
-    check_if_monday = ShortCircuitOperator(
-        task_id="check_if_monday", python_callable=check_if_monday
-    )
-
-    get_stock_non_diffusible = PythonOperator(
-        task_id="get_stock_non_diffusible",
-        python_callable=get_stock_non_diffusible,
-    )
-
-    send_stock_minio = PythonOperator(
-        task_id="send_stock_minio",
-        python_callable=send_stock_minio,
-    )
-
-    send_notification = PythonOperator(
-        task_id="send_notification",
-        python_callable=send_notification,
-    )
-
-    send_notification_stock = PythonOperator(
-        task_id="send_notification_stock",
-        python_callable=send_notification_stock,
-    )
-
-    get_current_flux_unite_legale.set_upstream(clean_previous_outputs)
-    get_current_flux_etablissement.set_upstream(get_current_flux_unite_legale)
-    get_current_flux_non_diffusible.set_upstream(get_current_flux_etablissement)
-    save_date_last_modified.set_upstream(get_current_flux_non_diffusible)
-    send_flux_minio.set_upstream(save_date_last_modified)
-    send_notification.set_upstream(send_flux_minio)
-    check_if_monday.set_upstream(send_flux_minio)
-    get_stock_non_diffusible.set_upstream(check_if_monday)
-    send_stock_minio.set_upstream(get_stock_non_diffusible)
-    send_notification_stock.set_upstream(send_stock_minio)
+# Instantiate the DAG
+data_processing_sirene_flux()
