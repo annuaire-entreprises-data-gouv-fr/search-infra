786dabe6dcdd5ef3353ff6c7968391d33839ec31
Hadrien Bossard <hadrien.bossard.ext@mail.numerique.gouv.fr>
2025-01-21 18:10:54 +0100

    refactor(etl.dag): use @dag and rename file
    PythonOperator still in use so we can easily identify the tasks that
    need to be refactored.
    
    All tasks instances had to be renamed because with @dag the instance's
    name is conflicting with the callable's name.

--- workflows/data_pipelines/etl/dag.py
+++ workflows/data_pipelines/etl/dag.py
@@ -1,9 +1,11 @@
 from datetime import datetime, timedelta
 
-from airflow.models import DAG
+from airflow.decorators import dag, task
 from airflow.operators.python import PythonOperator
-from operators.clean_folder import CleanFolderOperator
+from dag_datalake_sirene.helpers import Notification
 from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+
+from operators.clean_folder import CleanFolderOperator
 
 # fmt: off
 from dag_datalake_sirene.workflows.data_pipelines.etl.task_functions.\
@@ -20,7 +22,6 @@ from dag_datalake_sirene.workflows.data_pipelines.etl.task_functions.\
     replace_etablissement_table,
     replace_siege_table,
 )
-
 from dag_datalake_sirene.workflows.data_pipelines.etl.task_functions.\
     create_additional_data_tables import (
     create_agence_bio_table,
@@ -66,11 +67,6 @@ from dag_datalake_sirene.workflows.data_pipelines.etl.task_functions.\
     add_rne_data_to_unite_legale_table,
     add_ancien_siege_flux_data,
 )
-from dag_datalake_sirene.workflows.data_pipelines.etl.task_functions.send_notification\
-    import (
-    send_notification_success_tchap,
-    send_notification_failure_tchap,
-)
 from dag_datalake_sirene.workflows.data_pipelines.etl.task_functions.\
     create_json_last_modified import (
     create_data_source_last_modified_file,
@@ -100,260 +96,263 @@ default_args = {
     "retry_delay": timedelta(minutes=10),
 }
 
-with DAG(
+@dag(
     dag_id=AIRFLOW_ETL_DAG_NAME,
+    tags=["database", "all-data"],
     default_args=default_args,
-    schedule_interval="0 10 * * *",  # Run everyday at 10 am
+    schedule_interval="0 10 * * *",  # Run everyday at 10 am UTC
     start_date=datetime(2023, 12, 27),
     dagrun_timeout=timedelta(minutes=60 * 5),
-    tags=["database", "all-data"],
+    params={},
     catchup=False,  # False to ignore past runs
-    on_failure_callback=send_notification_failure_tchap,
+    on_failure_callback=Notification.send_notification_tchap,
+    on_success_callback=Notification.send_notification_tchap,
     max_active_runs=1,
-) as dag:
+)
+def extract_transform_load_db():
     clean_previous_folder = CleanFolderOperator(
         task_id="clean_previous_folder",
         folder_path=(f"{AIRFLOW_DAG_TMP}{AIRFLOW_DAG_FOLDER}{AIRFLOW_ETL_DAG_NAME}"),
     )
 
-    create_sqlite_database = PythonOperator(
+    create_sqlite_database_task = PythonOperator(
         task_id="create_sqlite_database",
         provide_context=True,
         python_callable=create_sqlite_database,
     )
 
-    create_unite_legale_table = PythonOperator(
+    create_unite_legale_table_task = PythonOperator(
         task_id="create_unite_legale_table",
         provide_context=True,
         python_callable=create_unite_legale_table,
     )
 
-    create_etablissement_table = PythonOperator(
+    create_etablissement_table_task = PythonOperator(
         task_id="create_etablissement_table",
         provide_context=True,
         python_callable=create_etablissement_table,
     )
 
-    create_flux_unite_legale_table = PythonOperator(
+    create_flux_unite_legale_table_task = PythonOperator(
         task_id="create_flux_unite_legale_table",
         provide_context=True,
         python_callable=create_flux_unite_legale_table,
     )
 
-    add_ancien_siege_flux_data = PythonOperator(
+    add_ancien_siege_flux_data_task = PythonOperator(
         task_id="add_ancien_siege_flux_data",
         provide_context=True,
         python_callable=add_ancien_siege_flux_data,
     )
 
-    create_flux_etablissement_table = PythonOperator(
+    create_flux_etablissement_table_task = PythonOperator(
         task_id="create_flux_etablissement_table",
         provide_context=True,
         python_callable=create_flux_etablissement_table,
         trigger_rule="all_done",
     )
 
-    replace_unite_legale_table = PythonOperator(
+    replace_unite_legale_table_task = PythonOperator(
         task_id="replace_unite_legale_table",
         provide_context=True,
         python_callable=replace_unite_legale_table,
         trigger_rule="all_done",
     )
 
-    replace_etablissement_table = PythonOperator(
+    replace_etablissement_table_task = PythonOperator(
         task_id="replace_etablissement_table",
         provide_context=True,
         python_callable=replace_etablissement_table,
     )
 
-    count_nombre_etablissement = PythonOperator(
+    count_nombre_etablissement_task = PythonOperator(
         task_id="count_nombre_etablissement",
         provide_context=True,
         python_callable=count_nombre_etablissement,
     )
 
-    count_nombre_etablissement_ouvert = PythonOperator(
+    count_nombre_etablissement_ouvert_task = PythonOperator(
         task_id="count_nombre_etablissement_ouvert",
         provide_context=True,
         python_callable=count_nombre_etablissement_ouvert,
     )
 
-    create_historique_unite_legale_table = PythonOperator(
+    create_historique_unite_legale_table_task = PythonOperator(
         task_id="create_historique_unite_legale_table",
         provide_context=True,
         python_callable=create_historique_unite_legale_tables,
     )
 
-    create_date_fermeture_unite_legale_table = PythonOperator(
+    create_date_fermeture_unite_legale_table_task = PythonOperator(
         task_id="create_date_fermeture_unite_legale_table",
         provide_context=True,
         python_callable=create_date_fermeture_unite_legale_table,
     )
 
-    insert_date_fermeture_unite_legale = PythonOperator(
+    insert_date_fermeture_unite_legale_task = PythonOperator(
         task_id="insert_date_fermeture_unite_legale",
         provide_context=True,
         python_callable=insert_date_fermeture_unite_legale,
     )
 
-    inject_rne_unite_legale_data = PythonOperator(
+    inject_rne_unite_legale_data_task = PythonOperator(
         task_id="add_rne_siren_data_to_unite_legale_table",
         provide_context=True,
         python_callable=add_rne_data_to_unite_legale_table,
     )
 
-    create_siege_table = PythonOperator(
+    create_siege_table_task = PythonOperator(
         task_id="create_siege_table",
         provide_context=True,
         python_callable=create_siege_table,
     )
 
-    replace_siege_table = PythonOperator(
+    replace_siege_table_task = PythonOperator(
         task_id="replace_siege_table",
         provide_context=True,
         python_callable=replace_siege_table,
     )
 
-    inject_rne_siege_data = PythonOperator(
+    inject_rne_siege_data_task = PythonOperator(
         task_id="add_rne_data_to_siege_table",
         provide_context=True,
         python_callable=add_rne_data_to_siege_table,
     )
 
-    create_historique_etablissement_table = PythonOperator(
+    create_historique_etablissement_table_task = PythonOperator(
         task_id="create_historique_etablissement_table",
         provide_context=True,
         python_callable=create_historique_etablissement_table,
         trigger_rule="all_done",
     )
 
-    create_date_fermeture_etablissement_table = PythonOperator(
+    create_date_fermeture_etablissement_table_task = PythonOperator(
         task_id="create_date_fermeture_etablissement_table",
         provide_context=True,
         python_callable=create_date_fermeture_etablissement_table,
     )
 
-    insert_date_fermeture_etablissement = PythonOperator(
+    insert_date_fermeture_etablissement_task = PythonOperator(
         task_id="insert_date_fermeture_etablissement",
         provide_context=True,
         python_callable=insert_date_fermeture_etablissement,
     )
 
-    get_latest_rne_database = PythonOperator(
+    get_latest_rne_database_task = PythonOperator(
         task_id="get_rne_database",
         provide_context=True,
         python_callable=get_latest_rne_database,
     )
 
-    create_dirig_pp_table = PythonOperator(
+    create_dirig_pp_table_task = PythonOperator(
         task_id="create_dirig_pp_table",
         provide_context=True,
         python_callable=create_dirig_pp_table,
     )
 
-    create_dirig_pm_table = PythonOperator(
+    create_dirig_pm_table_task = PythonOperator(
         task_id="create_dirig_pm_table",
         provide_context=True,
         python_callable=create_dirig_pm_table,
     )
 
-    create_benef_table = PythonOperator(
+    create_benef_table_task = PythonOperator(
         task_id="create_benef_table",
         provide_context=True,
         python_callable=create_benef_table,
     )
 
-    create_immatriculation_table = PythonOperator(
+    create_immatriculation_table_task = PythonOperator(
         task_id="copy_immatriculation_table",
         provide_context=True,
         python_callable=create_immatriculation_table,
     )
 
-    create_bilan_financier_table = PythonOperator(
+    create_bilan_financier_table_task = PythonOperator(
         task_id="create_bilan_financier_table",
         provide_context=True,
         python_callable=create_bilan_financier_table,
     )
 
-    create_convention_collective_table = PythonOperator(
+    create_convention_collective_table_task = PythonOperator(
         task_id="create_convention_collective_table",
         provide_context=True,
         python_callable=create_convention_collective_table,
     )
 
-    create_ess_table = PythonOperator(
+    create_ess_table_task = PythonOperator(
         task_id="create_ess_table",
         provide_context=True,
         python_callable=create_ess_table,
     )
 
-    create_rge_table = PythonOperator(
+    create_rge_table_task = PythonOperator(
         task_id="create_rge_table",
         provide_context=True,
         python_callable=create_rge_table,
     )
 
-    create_finess_table = PythonOperator(
+    create_finess_table_task = PythonOperator(
         task_id="create_finess_table",
         provide_context=True,
         python_callable=create_finess_table,
     )
 
-    create_agence_bio_table = PythonOperator(
+    create_agence_bio_table_task = PythonOperator(
         task_id="create_agence_bio_table",
         provide_context=True,
         python_callable=create_agence_bio_table,
     )
 
-    create_organisme_formation_table = PythonOperator(
+    create_organisme_formation_table_task = PythonOperator(
         task_id="create_organisme_formation_table",
         provide_context=True,
         python_callable=create_organisme_formation_table,
     )
 
-    create_uai_table = PythonOperator(
+    create_uai_table_task = PythonOperator(
         task_id="create_uai_table",
         provide_context=True,
         python_callable=create_uai_table,
     )
 
-    create_spectacle_table = PythonOperator(
+    create_spectacle_table_task = PythonOperator(
         task_id="create_spectacle_table",
         provide_context=True,
         python_callable=create_spectacle_table,
     )
 
-    create_egapro_table = PythonOperator(
+    create_egapro_table_task = PythonOperator(
         task_id="create_egapro_table",
         provide_context=True,
         python_callable=create_egapro_table,
     )
 
-    create_elu_table = PythonOperator(
+    create_elu_table_task = PythonOperator(
         task_id="create_elu_table",
         provide_context=True,
         python_callable=create_elu_table,
     )
 
-    create_colter_table = PythonOperator(
+    create_colter_table_task = PythonOperator(
         task_id="create_colter_table",
         provide_context=True,
         python_callable=create_colter_table,
     )
 
-    create_marche_inclusion_table = PythonOperator(
+    create_marche_inclusion_table_task = PythonOperator(
         task_id="create_marche_inclusion_table",
         provide_context=True,
         python_callable=create_marche_inclusion_table,
     )
 
-    send_database_to_minio = PythonOperator(
+    send_database_to_minio_task = PythonOperator(
         task_id="upload_db_to_minio",
         provide_context=True,
         python_callable=upload_db_to_minio,
     )
 
-    create_data_source_last_modified_file = PythonOperator(
+    create_data_source_last_modified_file_task = PythonOperator(
         task_id="create_data_source_last_modified_file",
         provide_context=True,
         python_callable=create_data_source_last_modified_file,
@@ -371,62 +370,57 @@ with DAG(
         deferrable=False,
     )
 
-    send_notification_tchap = PythonOperator(
-        task_id="send_notification_tchap",
-        python_callable=send_notification_success_tchap,
+    create_sqlite_database_task.set_upstream(clean_previous_folder)
+
+    create_unite_legale_table_task.set_upstream(create_sqlite_database_task)
+    create_historique_unite_legale_table_task.set_upstream(create_unite_legale_table_task)
+    create_date_fermeture_unite_legale_table_task.set_upstream(
+        create_historique_unite_legale_table_task
+    )
+    create_etablissement_table_task.set_upstream(create_date_fermeture_unite_legale_table_task)
+    create_flux_unite_legale_table_task.set_upstream(create_etablissement_table_task)
+    create_flux_etablissement_table_task.set_upstream(create_flux_unite_legale_table_task)
+    replace_unite_legale_table_task.set_upstream(create_flux_etablissement_table_task)
+    insert_date_fermeture_unite_legale_task.set_upstream(replace_unite_legale_table_task)
+    replace_etablissement_table_task.set_upstream(insert_date_fermeture_unite_legale_task)
+    count_nombre_etablissement_task.set_upstream(replace_etablissement_table_task)
+    count_nombre_etablissement_ouvert_task.set_upstream(count_nombre_etablissement_task)
+    create_siege_table_task.set_upstream(count_nombre_etablissement_ouvert_task)
+    replace_siege_table_task.set_upstream(create_siege_table_task)
+    add_ancien_siege_flux_data_task.set_upstream(replace_siege_table_task)
+    create_historique_etablissement_table_task.set_upstream(add_ancien_siege_flux_data_task)
+    create_date_fermeture_etablissement_table_task.set_upstream(
+        create_historique_etablissement_table_task
+    )
+    insert_date_fermeture_etablissement_task.set_upstream(
+        create_date_fermeture_etablissement_table_task
     )
 
-    create_sqlite_database.set_upstream(clean_previous_folder)
+    get_latest_rne_database_task.set_upstream(insert_date_fermeture_etablissement_task)
+    inject_rne_unite_legale_data_task.set_upstream(get_latest_rne_database_task)
+    inject_rne_siege_data_task.set_upstream(inject_rne_unite_legale_data_task)
+    create_dirig_pp_table_task.set_upstream(inject_rne_siege_data_task)
+    create_dirig_pm_table_task.set_upstream(create_dirig_pp_table_task)
+    create_benef_table_task.set_upstream(create_dirig_pm_table_task)
+    create_immatriculation_table_task.set_upstream(create_benef_table_task)
 
-    create_unite_legale_table.set_upstream(create_sqlite_database)
-    create_historique_unite_legale_table.set_upstream(create_unite_legale_table)
-    create_date_fermeture_unite_legale_table.set_upstream(
-        create_historique_unite_legale_table
-    )
-    create_etablissement_table.set_upstream(create_date_fermeture_unite_legale_table)
-    create_flux_unite_legale_table.set_upstream(create_etablissement_table)
-    create_flux_etablissement_table.set_upstream(create_flux_unite_legale_table)
-    replace_unite_legale_table.set_upstream(create_flux_etablissement_table)
-    insert_date_fermeture_unite_legale.set_upstream(replace_unite_legale_table)
-    replace_etablissement_table.set_upstream(insert_date_fermeture_unite_legale)
-    count_nombre_etablissement.set_upstream(replace_etablissement_table)
-    count_nombre_etablissement_ouvert.set_upstream(count_nombre_etablissement)
-    create_siege_table.set_upstream(count_nombre_etablissement_ouvert)
-    replace_siege_table.set_upstream(create_siege_table)
-    add_ancien_siege_flux_data.set_upstream(replace_siege_table)
-    create_historique_etablissement_table.set_upstream(add_ancien_siege_flux_data)
-    create_date_fermeture_etablissement_table.set_upstream(
-        create_historique_etablissement_table
-    )
-    insert_date_fermeture_etablissement.set_upstream(
-        create_date_fermeture_etablissement_table
-    )
+    create_bilan_financier_table_task.set_upstream(create_immatriculation_table_task)
+    create_convention_collective_table_task.set_upstream(create_bilan_financier_table_task)
+    create_ess_table_task.set_upstream(create_convention_collective_table_task)
+    create_rge_table_task.set_upstream(create_ess_table_task)
+    create_finess_table_task.set_upstream(create_rge_table_task)
+    create_organisme_formation_table_task.set_upstream(create_agence_bio_table_task)
+    create_uai_table_task.set_upstream(create_organisme_formation_table_task)
+    create_spectacle_table_task.set_upstream(create_uai_table_task)
+    create_egapro_table_task.set_upstream(create_spectacle_table_task)
+    create_colter_table_task.set_upstream(create_egapro_table_task)
+    create_elu_table_task.set_upstream(create_colter_table_task)
+    create_marche_inclusion_table_task.set_upstream(create_elu_table_task)
 
-    get_latest_rne_database.set_upstream(insert_date_fermeture_etablissement)
-    inject_rne_unite_legale_data.set_upstream(get_latest_rne_database)
-    inject_rne_siege_data.set_upstream(inject_rne_unite_legale_data)
-    create_dirig_pp_table.set_upstream(inject_rne_siege_data)
-    create_dirig_pm_table.set_upstream(create_dirig_pp_table)
-    create_benef_table.set_upstream(create_dirig_pm_table)
-    create_immatriculation_table.set_upstream(create_benef_table)
-
-    create_bilan_financier_table.set_upstream(create_immatriculation_table)
-    create_convention_collective_table.set_upstream(create_bilan_financier_table)
-    create_ess_table.set_upstream(create_convention_collective_table)
-    create_rge_table.set_upstream(create_ess_table)
-    create_finess_table.set_upstream(create_rge_table)
-    create_agence_bio_table.set_upstream(create_finess_table)
-    create_organisme_formation_table.set_upstream(create_agence_bio_table)
-    create_uai_table.set_upstream(create_organisme_formation_table)
-    create_spectacle_table.set_upstream(create_uai_table)
-    create_egapro_table.set_upstream(create_spectacle_table)
-    create_colter_table.set_upstream(create_egapro_table)
-    create_elu_table.set_upstream(create_colter_table)
-    create_marche_inclusion_table.set_upstream(create_elu_table)
-
-    send_database_to_minio.set_upstream(create_marche_inclusion_table)
-    create_data_source_last_modified_file.set_upstream(send_database_to_minio)
-    clean_folder.set_upstream(create_data_source_last_modified_file)
+    send_database_to_minio_task.set_upstream(create_marche_inclusion_table_task)
+    create_data_source_last_modified_file_task.set_upstream(send_database_to_minio_task)
+    clean_folder.set_upstream(create_data_source_last_modified_file_task)
 
     trigger_indexing_dag.set_upstream(clean_folder)
-    send_notification_tchap.set_upstream(trigger_indexing_dag)
+
+extract_transform_load_db()
