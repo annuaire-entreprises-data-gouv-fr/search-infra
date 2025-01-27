c5638935491c88bd6f2cc8638d275954b684a975
Hadrien Bossard <hadrien.bossard.ext@mail.numerique.gouv.fr>
2025-01-27 15:57:31 +0100

    refactor(etl.finess): use DatabaseTableConstructor

--- workflows/data_pipelines/etl/data_fetch_clean/finess.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from dag_datalake_sirene.workflows.data_pipelines.finess.config import FINESS_CONFIG
-
-
-def preprocess_finess_data(data_dir):
-    df_finess = pd.read_csv(FINESS_CONFIG.url_minio, dtype=str)
-    return df_finess
