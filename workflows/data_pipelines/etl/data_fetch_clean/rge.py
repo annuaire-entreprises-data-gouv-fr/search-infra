5133369eed4981134e1e51fac0fadcc507090568
Hadrien Bossard <hadrien.bossard.ext@mail.numerique.gouv.fr>
2025-01-27 15:57:44 +0100

    refactor(etl.rge): use DatabaseTableConstructor

--- workflows/data_pipelines/etl/data_fetch_clean/rge.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from dag_datalake_sirene.workflows.data_pipelines.rge.config import RGE_CONFIG
-
-
-def preprocess_rge_data(data_dir):
-    df_rge = pd.read_csv(RGE_CONFIG.url_minio, dtype=str)
-    return df_rge
