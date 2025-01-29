24ff866daae0f482c836ec9f1af5c7e77e4d6427
HAEKADI <hajar.aitelkadi@proton.me>
2025-01-29 13:25:29 +0100

    fix: convention collective minio url in etl

--- workflows/data_pipelines/etl/data_fetch_clean/convention_collective.py
+++ workflows/data_pipelines/etl/data_fetch_clean/convention_collective.py
@@ -1,10 +1,12 @@
 import pandas as pd
-from dag_datalake_sirene.config import URL_MINIO_CONVENTION_COLLECTIVE
+from dag_datalake_sirene.workflows.data_pipelines.convcollective.config import (
+    CONVENTION_COLLECTIVE_CONFIG,
+)
 
 
 def preprocess_convcollective_data(data_dir):
     df_cc = pd.read_csv(
-        URL_MINIO_CONVENTION_COLLECTIVE,
+        CONVENTION_COLLECTIVE_CONFIG.url_minio,
         dtype=str,
     )
     return df_cc
