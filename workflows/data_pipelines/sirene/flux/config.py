8c060f160fb4e1adbe2ff58910531544a06af989
Hadrien Bossard <hadrien.bossard.ext@mail.numerique.gouv.fr>
2025-04-24 14:56:47 +0200

    fix(flux.sirene): minio path

--- workflows/data_pipelines/sirene/flux/config.py
+++ workflows/data_pipelines/sirene/flux/config.py
@@ -9,8 +9,8 @@ FLUX_SIRENE_CONFIG = DataSourceConfig(
     name="flux_api_sirene",
     tmp_folder=f"{DataSourceConfig.base_tmp_folder}/sirene/flux/",
     minio_path="insee/flux/",
-    url_minio=f"{MINIO_BASE_URL}insee/flux/latest/",
-    url_minio_metadata=f"{MINIO_BASE_URL}insee/flux/latest/metadata.json",
+    url_minio=f"{MINIO_BASE_URL}insee/flux/",
+    url_minio_metadata=f"{MINIO_BASE_URL}insee/flux/metadata.json",
     url_api="https://api.insee.fr/api-sirene/3.11/",
     auth_api=Variable.get("SECRET_BEARER_INSEE", None),
 )
