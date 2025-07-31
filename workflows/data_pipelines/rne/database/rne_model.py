2e1b4287c8dc91a74e4cdc3c93a118da402dc50e
HAEKADI <hajar.aitelkadi@proton.me>
2025-07-31 23:28:11 +0200

    feat: add new fields to rne model

--- workflows/data_pipelines/rne/database/rne_model.py
+++ workflows/data_pipelines/rne/database/rne_model.py
@@ -17,6 +17,18 @@ class Adresse(BaseModel):
     distributionSpeciale: str | None = None
 
 
+class EntrepriseDomiciliataire(BaseModel):
+    siren: str | None = None
+    denomination: str | None = None
+    nomCommercial: str | None = None
+    formeJuridique: str | None = None
+
+
+class AdresseEntreprise(BaseModel):
+    adresse: Adresse | None = None
+    entrepriseDomiciliataire: EntrepriseDomiciliataire | None = None
+
+
 class Description(BaseModel):
     montantCapital: float | None = None
     capitalVariable: bool | None = None
@@ -26,15 +38,32 @@ class Description(BaseModel):
 
 
 class DescriptionPersonne(BaseModel):
-    dateDeNaissance: str | None = None
+    role: str | None = None
     nom: str | None = None
-    nomUsage: str | None = None
     prenoms: list[str] | None = None
     genre: str | None = None
     titre: str | None = None
-    role: str | None = None
+    nomUsage: str | None = None
+    dateDeNaissance: str | None = None
+    paysNaissance: str | None = None
+    lieuDeNaissance: str | None = None
     nationalite: str | None = None
     situationMatrimoniale: str | None = None
+
+
+class Modalite(BaseModel):
+    dateEffet: str | None = None
+    detentionPartDirecte: bool | None = None
+
+
+class Beneficiaire(BaseModel):
+    descriptionPersonne: DescriptionPersonne | None = None
+    adresseDomicile: Adresse | None = None
+
+
+class BeneficiaireEffectif(BaseModel):
+    beneficiaire: Beneficiaire | None = None
+    modalite: Modalite | None = None
 
 
 class PouvoirIndividu(BaseModel):
@@ -53,11 +82,6 @@ class PouvoirEntreprise(BaseModel):
     nomCommercial: str | None = None
 
 
-class Entrepreneur(BaseModel):
-    descriptionPersonne: DescriptionPersonne | None = DescriptionPersonne()
-    adresseDomicile: Adresse | None = Adresse()
-
-
 class Pouvoir(BaseModel):
     roleEntreprise: str | None = None
     libelleRoleEntreprise: str | None = None
@@ -71,104 +95,177 @@ class Composition(BaseModel):
     pouvoirs: list[Pouvoir] | None = Pouvoir()
 
 
-class Entreprise(BaseModel):
-    denomination: str | None = None
-    formeJuridique: str | None = None
-    nomCommercial: str | None = None
-    effectifSalarie: str | None = None
-    dateImmat: str | None = None
-    codeApe: str | None = None
-    indicateurAssocieUnique: bool | None = None
-    dateDebutActiv: str | None = None
-
-
-class Identite(BaseModel):
-    entreprise: Entreprise | None = Entreprise()
-    entrepreneur: Entrepreneur | None = Entrepreneur()
-    description: Description | None = Description()
-
-
-class AdresseEntreprise(BaseModel):
-    caracteristiques: dict | None = None
-    adresse: Adresse | None = Adresse()
-    entrepriseDomiciliataire: dict | None = None
-
-
 class DescriptionEtablissement(BaseModel):
     siret: str | None = None
     enseigne: str | None = None
     nomCommercial: str | None = None
+    dateEffet: str | None = None
+    dateEffetFermeture: str | None = None
+    nomEtablissement: str | None = None
+    rolePourEntreprise: str | None = None  # siège, principal ou secondaire
+    codeApe: str | None = None
+    statutOuvertFerme: str | None = None
+    indicateurEtablissementPrincipal: bool | None = None
 
 
 class Activite(BaseModel):
-    categoryCode: str | None = None
     indicateurPrincipal: bool | None = None
+    indicateurArtisteAuteur: bool | None = None
+    indicateurMarinProfessionnel: bool | None = None
+    metierArt: bool | None = None
+    activiteMicro: bool | None = None
+    indicateurActiviteViticole: bool | None = None
+    indicateurActiviteElevage: bool | None = None
     indicateurProlongement: bool | None = None
-    dateDebut: date | None = None
+    activiteReguliere: str | None = None
+    dateDebut: str | None = None
+    dateFin: str | None = None
+    codeAprm: str | None = None
+    descriptionDetaillee: str | None = None
     formeExercice: str | None = None
     categorisationActivite1: str | None = None
     categorisationActivite2: str | None = None
     categorisationActivite3: str | None = None
-    indicateurActiviteeApe: bool | None = None
+    categorisationActivite4: str | None = None
+    categoryCode: str | None = None
     codeApe: str | None = None
-    activiteRattacheeEirl: bool | None = None
+    qualiteNonSedentaire: str | None = None
+    indicateurNonSedentaire: bool | None = None
+    statutPraticien: str | None = None  # médecin
+    precisionActivite: str | None = None
+    numPraticien: str | None = None
 
 
-class EtablissementPrincipal(BaseModel):
-    descriptionEtablissement: DescriptionEtablissement | None = (
-        DescriptionEtablissement()
-    )
-    adresse: Adresse | None = Adresse()
-    activites: list[Activite] | None = Activite()
+class Domaine(BaseModel):
+    nomDomaine: str | None = None
 
 
-class DetailCessationEntreprise(BaseModel):
-    dateCessationTotaleActivite: date | None = None
-    dateEffet: date | None = None
-    dateRadiation: date | None = None
+class DetailCessation(BaseModel):
+    dateRadiation: str | None = None
+    detailMotifCessation: str | None = None
+    motifCessation: str | None = None
+    dateCessationActiviteSalariee: str | None = None
+    dateCessationTotaleActivite: str | None = None
+    dateMiseEnSommeil: str | None = None
+    indicateurDissolution: bool | None = None
+    typeDissolution: str | None = None
+    indicateurCessationTemporaire: bool | None = None
+    indicateurDisparitionPM: bool | None = None
 
 
-class NatureCessationEntreprise(BaseModel):
-    etatAdministratifInsee: str | None = None
+class Etablissement(BaseModel):
+    descriptionEtablissement: DescriptionEtablissement | None = None
+    adresse: Adresse | None = None
+    activites: list[Activite] | None = None
+    nomsDeDomaine: list[Domaine] | None = None
+    detailCessationEtablissement: DetailCessation | None = None
+    isPrincipal: bool | None = None
+
+
+class Contact(BaseModel):
+    mail: str | None = None
+    telephone: str | None = None
+
+
+class Conjoint(BaseModel):
+    contact: Contact | None = None
+    descriptionPersonne: DescriptionPersonne | None = None
+    adresseDomicile: Adresse | None = None
+
+
+class Entrepreneur(BaseModel):
+    roleConjoint: str | None = None
+    conjoint: Conjoint | None = None
+    descriptionPersonne: DescriptionPersonne | None = None
+    adresseDomicile: Adresse | None = None
+    qualiteArtisan: str | None = None
+    contact: Contact | None = None
+
+
+class Entreprise(BaseModel):
+    formeJuridique: str | None = None
+    codeApe: str | None = None
+    denomination: str | None = None
+    nomCommercial: str | None = None
+    nomExploitation: str | None = None
+    effectifSalarie: str | None = None
+    effectifApprenti: str | None = None
+    dateImmat: str | None = None
+    dateDebutActiv: str | None = None
+
+
+class Identite(BaseModel):
+    entreprise: Entreprise | None = None
+    entrepreneur: Entrepreneur | None = None  # Personne Physique uniquement
+    description: Description | None = None  ###### # Personne Morale uniquement
 
 
 class Exploitation(BaseModel):
     identite: Identite | None = Identite()
     composition: Composition | None = None
     adresseEntreprise: AdresseEntreprise | None = AdresseEntreprise()
-    etablissementPrincipal: EtablissementPrincipal | None = EtablissementPrincipal()
-    autresEtablissements: list[EtablissementPrincipal] | None = [
-        EtablissementPrincipal()
-    ]
-    detailCessationEntreprise: DetailCessationEntreprise | None = (
-        DetailCessationEntreprise()
-    )
+    etablissementPrincipal: Etablissement | None = Etablissement()
+    autresEtablissements: list[Etablissement] | None = [Etablissement()]
+    detailCessationEntreprise: DetailCessation | None = DetailCessation()
 
 
 class PersonneMorale(BaseModel):
     identite: Identite | None = Identite()
     composition: Composition | None = None
     adresseEntreprise: AdresseEntreprise | None = AdresseEntreprise()
-    etablissementPrincipal: EtablissementPrincipal | None = EtablissementPrincipal()
-    autresEtablissements: list[EtablissementPrincipal] | None = [
-        EtablissementPrincipal()
-    ]
-    detailCessationEntreprise: DetailCessationEntreprise | None = (
-        DetailCessationEntreprise()
-    )
+    etablissementPrincipal: Etablissement | None = Etablissement()
+    autresEtablissements: list[Etablissement] | None = [Etablissement()]
+    detailCessationEntreprise: DetailCessation | None = DetailCessation()
 
 
 class PersonnePhysique(BaseModel):
-    identite: Identite | None = Identite()
-    composition: Composition | None = None
-    adresseEntreprise: AdresseEntreprise | None = AdresseEntreprise()
-    etablissementPrincipal: EtablissementPrincipal | None = EtablissementPrincipal()
-    autresEtablissements: list[EtablissementPrincipal] | None = [
-        EtablissementPrincipal()
-    ]
-    detailCessationEntreprise: DetailCessationEntreprise | None = (
-        DetailCessationEntreprise()
-    )
+    identite: Identite | None = None
+    adresseEntreprise: AdresseEntreprise | None = None
+    etablissementPrincipal: Etablissement | None = None
+    autresEtablissements: list[Etablissement] | None = None
+    detailCessationEntreprise: DetailCessation | None = None
+    beneficiairesEffectifs: list[BeneficiaireEffectif] | None = None
+    composition: Composition | None = None  ####
+
+
+class RAA(BaseModel):
+    estPresent: bool | None
+    dateDebut: str | None
+    dateFin: str | None
+
+
+class RNM(BaseModel):
+    estPresent: bool | None = None
+    dateDebut: str | None = None
+    dateFin: str | None = None
+
+
+class RNCS(BaseModel):
+    estPresent: bool | None = None
+    dateDebut: str | None = None
+    dateFin: str | None = None
+
+
+class RegistreAnterieur(BaseModel):
+    raa: RAA | None = None
+    rnm: RNM | None = None
+    rncs: RNCS | None = None
+
+
+class Mention(BaseModel):
+    numObservation: str | None = None
+    dateEffet: str | None = None
+    texte: str | None = None
+
+
+class InscriptionOffice(BaseModel):
+    event: str | None = None
+    dateEffet: str | None = None
+    observation: str | None = None
+    partnerCenter: str | None = None
+    partnerCode: str | None = None
+    observationComplementaire: str | None
+    mentions: list[Mention] | None = None  # schéma privé
 
 
 class NatureCreation(BaseModel):
@@ -177,37 +274,64 @@ class NatureCreation(BaseModel):
     formeJuridiqueInsee: str | None = None
     societeEtrangere: bool | None = None
     entrepriseAgricole: bool | None = None
+    eirl: bool | None = None
+    typeExploitation: str | None = None
+    microEntreprise: bool | None = None
+    relieeEntrepriseAgricole: bool | None = None
+
+
+class NatureCessationEntreprise(BaseModel):
+    dateRadiation: date | None = None
+    etatAdministratifInsee: str | None = None
 
 
 class Content(BaseModel):
+    evenementCessation: str | None = None
+    natureCessation: str | None = None
+    natureCessationEntreprise: NatureCessationEntreprise | None = None
+    succursaleOuFiliale: str | None = None
     formeExerciceActivitePrincipale: str | None = None
-    personnePhysique: PersonnePhysique | None = None
-    personneMorale: PersonneMorale | None = None
-    exploitation: Exploitation | None = None
-    natureCessationEntreprise: NatureCessationEntreprise | None = (
-        NatureCessationEntreprise()
-    )
-    natureCreation: NatureCreation | None = NatureCreation()
+    natureCreation: NatureCreation | None = None
+    personnePhysique: PersonnePhysique | None = None  ########
+    personneMorale: PersonneMorale | None = None  ########
+    exploitation: Exploitation | None = None  ########
+    registreAnterieur: RegistreAnterieur | None = None
+    inscriptionsOffices: list[InscriptionOffice] | None = None
+
+
+class Historique(BaseModel):
+    dateIntegration: str | None = None
+    codeEvenement: str | None = None
+    libelleEvenement: str | None = None
+    numeroLiasse: str | None = None
+    patchId: str | None = None
+    dateEffet: str | None = None
+    cheminDateEffet: str | None = None
+    cheminDateEffetId: str | None = None
 
 
 class Formality(BaseModel):
     siren: str
-    companyName: str | None = None
     content: Content
-    typePersonne: str | None = None
-    formeJuridique: str | None = None
     diffusionINSEE: str | None = None
-    codeAPE: str | None = None
+    typePersonne: str | None = None
+    codeAPE: str | None = None  # schéma privé
+    diffusionCommerciale: bool | None = None
+    historique: Historique
+    formeJuridique: str | None = None
+    numRna: str | None = None
     created: datetime | None = None
     updated: datetime | None = None
 
 
 class RNECompany(BaseModel):
-    updatedAt: datetime | None = None
     createdAt: datetime | None = None
+    updatedAt: datetime | None = None
+    nombreEtablissementsOuverts: int | None = None
+    nombreRepresentantsActifs: int | None = None
     formality: Formality
     siren: str
-    origin: str | None = None
+    sirenDoublons: str | None = None
 
     def is_personne_morale(self) -> bool:
         return isinstance(self.formality.content.personneMorale, PersonneMorale)
