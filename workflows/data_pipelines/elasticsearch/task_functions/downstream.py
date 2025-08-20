85cda712e85d2b492c823d7a860fc02866371fc8
Paul Lesur <paul.lesur@free.fr>
2025-08-20 15:35:06 +0200

    feat(dag_snapshot): move alias update to dag instead of import script on hosts

--- workflows/data_pipelines/elasticsearch/task_functions/downstream.py
+++ workflows/data_pipelines/elasticsearch/task_functions/downstream.py
@@ -70,3 +70,38 @@ def wait_for_downstream_index_import(elastic_index):
 
     if len(pending) > 0:
         raise Exception("Downstream import is taking too long")
+
+def update_downstream_alias(**kwargs):
+    connections.create_connection(
+        hosts=[ELASTIC_DOWNSTREAM_URLS],
+        http_auth=(ELASTIC_DOWNSTREAM_USER, ELASTIC_DOWNSTREAM_PASSWORD),
+        retry_on_timeout=True,
+    )
+    elastic_connection = connections.get_connection()
+    alias = "siren-reader"
+    elastic_index = kwargs["ti"].xcom_pull(
+        key="elastic_index",
+        task_ids="get_next_index_name",
+        dag_id=AIRFLOW_ELK_DAG_NAME,
+        include_prior_dates=True,
+    )
+    indices = []
+    try:
+        config = elastic_connection.indices.get_alias(name=alias)
+        indices = config.keys() if config is not None else []
+    except NotFoundError:
+        pass
+    actions = [
+        {
+            "remove": {
+                "index": index,
+                "alias": alias,
+            }
+        }
+        for index in indices
+    ]
+    actions.append({"add": {"index": elastic_index, "alias": alias}})
+    logging.info(
+        f"Updating alias siren-reader : add {elastic_index}, remove {', '.join(indices)}"
+    )
+    elastic_connection.indices.update_aliases({"actions": actions})
