fc0fcbc1b4cf9a177d49044745766247f5d90fdd
Hadrien Bossard <hadrien.bossard.ext@mail.numerique.gouv.fr>
2025-10-20 18:35:25 +0200

    feat(finess) Implement finess juridique dag

---
+++ workflows/data_pipelines/finess/juridique/README.md
@@ -0,0 +1,11 @@
+# Documentation
+
+## data_processing_finess
+
+| Information | Valeur |
+| -------- | -------- |
+| Fichier source | `dag.py` |
+| Description | Ce traitement permet de récupérer les unités légales du domaine sanitaire et social. |
+| Fréquence | Quotidienne |
+| Données sources | [Extraction Finess des Entreprises](https://data.gouv.fr/datasets/5369956ba3a729239d2046fc) |
+| Données de sorties | Minio |
---
+++ workflows/data_pipelines/finess/juridique/processor.py
@@ -0,0 +1,63 @@
+import pandas as pd
+from dag_datalake_sirene.helpers import DataProcessor, Notification
+from dag_datalake_sirene.workflows.data_pipelines.finess.juridique.config import (
+    FINESS_JURIDIQUE_CONFIG,
+)
+
+
+class FinessJuridiqueProcessor(DataProcessor):
+    def __init__(self):
+        super().__init__(FINESS_JURIDIQUE_CONFIG)
+
+    def preprocess_data(self):
+        df_finess_geographique = (
+            pd.read_csv(
+                self.config.files_to_download["finess_geographique"]["destination"],
+                dtype=str,
+                sep=";",
+                encoding="Latin-1",
+                skiprows=1,
+                header=None,
+            )
+            .rename(columns={2: "finess_juridique", 22: "siret"})
+            .filter(["finess_juridique", "siret"])
+            .assign(siren=lambda df: df["siret"].str[:9])
+            .filter(["finess_juridique", "siren"])
+            .loc[lambda x: x["siren"].notna()]
+        )
+
+        df_finess_juridique = (
+            pd.read_csv(
+                self.config.files_to_download["finess_juridique"]["destination"],
+                dtype=str,
+                sep=";",
+                encoding="Latin-1",
+                skiprows=1,
+                header=None,
+            )
+            .rename(columns={1: "finess_juridique", 20: "siren"})
+            .filter(["finess_juridique", "siren"])
+            .loc[lambda x: x["siren"].notna()]
+        )
+
+        df_list_finess_juridique = (
+            pd.concat(
+                [df_finess_geographique, df_finess_juridique],
+                ignore_index=True,
+            )
+            .groupby(["siren"], as_index=False)
+            .agg(
+                liste_finess_juridique=(
+                    "finess_juridique",
+                    lambda x: str(list(x.unique())),
+                )
+            )
+        )
+        df_list_finess_juridique.to_csv(
+            f"{self.config.tmp_folder}/finess_juridique.csv", index=False
+        )
+
+        DataProcessor.push_message(
+            Notification.notification_xcom_key,
+            column=df_list_finess_juridique["siren"],
+        )
