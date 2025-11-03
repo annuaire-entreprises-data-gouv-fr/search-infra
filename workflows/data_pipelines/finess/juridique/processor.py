f41a640a0afe57c237d706a2f06c571bdf46e488
Hadrien Bossard <hadrien.bossard.ext@mail.numerique.gouv.fr>
2025-11-03 18:28:08 +0100

    chore(finess): add comments

--- workflows/data_pipelines/finess/juridique/processor.py
+++ workflows/data_pipelines/finess/juridique/processor.py
@@ -10,7 +10,19 @@ class FinessJuridiqueProcessor(DataProcessor):
         super().__init__(FINESS_JURIDIQUE_CONFIG)
 
     def preprocess_data(self):
+        """
+        Create a list of all the Finess Juridique IDs of a Siren
+        including the ones from any of the établissement of the Siren
+        but excluding any Finess Juridique without a Finess Géographique.
+
+        CSV Output columns:
+            - siren
+            - liste_finess_juridique: list of Finess Juridique of the siren
+            - has_finess_from_geographique_only: True if at least one Finess
+                comes from a Siret and not from the Siren
+        """
         df_finess_geographique = (
+            # Load the data..
             pd.read_csv(
                 self.config.files_to_download["finess_geographique"]["destination"],
                 dtype=str,
@@ -19,6 +31,7 @@ class FinessJuridiqueProcessor(DataProcessor):
                 skiprows=1,
                 header=None,
             )
+            # And clean it
             .rename(columns={2: "finess_juridique", 22: "siret"})
             .filter(["finess_juridique", "siret"])
             .assign(
@@ -29,6 +42,7 @@ class FinessJuridiqueProcessor(DataProcessor):
             .loc[lambda x: x["siren"].notna()]
         )
 
+        # Only keep Finess Juridique IDs that have at least one Finess Géographique
         valid_finess_ids = df_finess_geographique["finess_juridique"].unique()
 
         df_finess_juridique = (
@@ -44,18 +58,24 @@ class FinessJuridiqueProcessor(DataProcessor):
             .filter(["finess_juridique", "siren"])
             .assign(source=lambda df: "juridique")
             .loc[lambda x: x["siren"].notna()]
+            # Exclude any Finess Juridique with no Finess Géographique
             .loc[lambda x: x["finess_juridique"].isin(valid_finess_ids)]
         )
 
+        # Concat the Finess Juridique from both files
         df_combined = pd.concat(
             [df_finess_geographique, df_finess_juridique],
             ignore_index=True,
         )
 
+        # Get all entries where at least one Finess Juridique of a Siren comes from
+        # one of its Siret without being registered at the Siren level
+        # Used to calculate the has_finess_from_geographique_only flag
         finess_geographique_only = set(
             df_combined[df_combined["source"] == "geographique"]["finess_juridique"]
         ) - set(df_combined[df_combined["source"] == "juridique"]["finess_juridique"])
 
+        # Deduplicate the Finess Juridiques and calculate the flag
         df_list_finess_juridique = df_combined.groupby(["siren"], as_index=False).agg(
             liste_finess_juridique=(
                 "finess_juridique",
