4723596f94958d04776ffa686111b127e51681b1
Hadrien Bossard <hadrien.bossard.ext@mail.numerique.gouv.fr>
2026-01-16 10:41:11 +0100

    refactor(elasticsearch): use @task, @dag and Notification class

--- tests/e2e_tests/run_tests.py
+++ tests/e2e_tests/run_tests.py
@@ -1,6 +1,8 @@
 import pytest
+from airflow.sdk import task
 
 
+@task
 def run_e2e_tests():
     exit_code = pytest.main(["dags/data_pipelines_annuaire/tests/e2e_tests", "-vv"])
     if exit_code > 0:
--- workflows/data_pipelines/elasticsearch/task_functions/source_updates.py
+++ workflows/data_pipelines/elasticsearch/task_functions/source_updates.py
@@ -1,12 +1,15 @@
 import logging
 
+from airflow.sdk import task
+
 from data_pipelines_annuaire.config import (
     MINIO_DATA_SOURCE_UPDATE_DATES_FILE,
 )
 from data_pipelines_annuaire.helpers.minio_helpers import MinIOClient
 
 
-def sync_data_source_updates():
+@task
+def sync_data_source_updates_file():
     logging.info("Copying data sources' last update JSON file!!!")
 
     MinIOClient().copy_file(
