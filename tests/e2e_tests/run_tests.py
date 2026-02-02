f7781e106474e3bcb619049d70de69501d0dd495
Hadrien Bossard <hadrien.bossard.ext@mail.numerique.gouv.fr>
2026-02-02 17:07:22 +0100

    wip

--- tests/e2e_tests/run_tests.py
+++ tests/e2e_tests/run_tests.py
@@ -4,6 +4,8 @@ from airflow.sdk import task
 
 @task
 def run_e2e_tests():
-    exit_code = pytest.main(["dags/data_pipelines_annuaire/tests/e2e_tests", "-vv"])
+    exit_code = pytest.main(
+        ["dags/data_pipelines_annuaire/tests/e2e_tests", "-vv", "--log-cli-level=DEBUG"]
+    )
     if exit_code > 0:
         raise RuntimeError("Pytest Fail!!!!!")
