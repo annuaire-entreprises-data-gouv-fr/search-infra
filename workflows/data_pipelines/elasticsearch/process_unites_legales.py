f232e4219ac47f7807af6e5f2f23a63baf31065b
Hadrien Bossard <hadrien.bossard.ext@mail.numerique.gouv.fr>
2026-05-11 11:22:54 +0200

    revert: v1 of procédures collectives and new bodacc format in index (#794)
    Temporarly reverts commit e7987dcdf8feffbc4fe4951ca0e2b330a724552b.

--- workflows/data_pipelines/elasticsearch/process_unites_legales.py
+++ workflows/data_pipelines/elasticsearch/process_unites_legales.py
@@ -286,24 +286,10 @@ def process_unites_legales(chunk_unites_legales_sqlite):
         bodacc = unite_legale.get("bodacc")
         if bodacc:
             bodacc_data = json.loads(bodacc)
-            est_radie = sqlite_str_to_bool(bodacc_data.get("radiation_est_radie", 0))
-            procedure_colletive_statut = bodacc_data.get("procedure_collective_statut")
-            unite_legale_processed["bodacc"] = {
-                "radiation": {
-                    "est_radie": est_radie,
-                    "id_annonce": bodacc_data.get("radiation_id_annonce"),
-                    "date": bodacc_data.get("radiation_date"),
-                }
-                if est_radie
-                else None,
-                "procedure_collective": {
-                    "statut": procedure_colletive_statut,
-                    "id_annonce": bodacc_data.get("procedure_collective_id_annonce"),
-                    "date": bodacc_data.get("procedure_collective_date"),
-                }
-                if procedure_colletive_statut
-                else None,
-            }
+            bodacc_data["radiation_rcs"] = sqlite_str_to_bool(
+                bodacc_data.get("radiation_rcs", 0)
+            )
+            unite_legale_processed["bodacc"] = bodacc_data
         else:
             unite_legale_processed["bodacc"] = {}
 
