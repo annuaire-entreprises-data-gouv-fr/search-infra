b5d2d5fcd82af2cdbd5e74c40467a48daf95303c
Hajar AEK <48837850+HAEKADI@users.noreply.github.com>
2026-06-12 08:42:44 +0200

    feat(tva): add preprocessing dag (#841)
    closes #781

--- workflows/data_pipelines/tva/README.md
+++ workflows/data_pipelines/tva/README.md
@@ -2,4 +2,10 @@
 
 ## data_processing_tva
 
-TBD (for now sample file used)
+| Information | Valeur |
+| -------- | -------- |
+| Fichier source | `dag.py` |
+| Description | Ce traitement permet de récupérer la liste des numéros de TVA intracommunautaire français actifs. |
+| Fréquence | Quotidienne |
+| Données sources | [Numéros de TVA Intracommunautaire français](https://data.economie.gouv.fr/explore/assets/numeros-de-tva-intracommunautaires-francais/) |
+| Données de sorties | Object Storage |
--- workflows/data_pipelines/tva/config.py
+++ workflows/data_pipelines/tva/config.py
@@ -1,4 +1,5 @@
 from data_pipelines_annuaire.config import (
+    DATA_GOUV_BASE_URL,
     OBJECT_STORAGE_BASE_URL,
     DataSourceConfig,
 )
@@ -8,6 +9,13 @@ TVA_CONFIG = DataSourceConfig(
     tmp_folder=f"{DataSourceConfig.base_tmp_folder}/tva",
     object_storage_path="tva",
     file_name="tva",
+    files_to_download={
+        "tva": {
+            "url": f"{DATA_GOUV_BASE_URL}5199cd40-0e9c-4a24-8ba3-c2365999b2aa",
+            "resource_id": "5199cd40-0e9c-4a24-8ba3-c2365999b2aa",
+            "destination": f"{DataSourceConfig.base_tmp_folder}/tva/tva-download.csv",
+        }
+    },
     url_object_storage=f"{OBJECT_STORAGE_BASE_URL}tva/latest/tva.csv",
     url_object_storage_metadata=f"{OBJECT_STORAGE_BASE_URL}tva/latest/metadata.json",
     table_ddl="""
