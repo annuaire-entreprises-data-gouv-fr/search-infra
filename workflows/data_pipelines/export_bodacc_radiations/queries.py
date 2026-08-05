bcf320c94ca255edfb6512b3e6e4495e03189366
Hadrien Bossard <hadrien.bossard.ext@mail.numerique.gouv.fr>
2026-08-05 17:06:19 +0200

    feat(export): add rne information to bodacc export

--- workflows/data_pipelines/export_bodacc_radiations/queries.py
+++ workflows/data_pipelines/export_bodacc_radiations/queries.py
@@ -1,44 +1,67 @@
-# Entreprises individuelles radiées au RCS d'après le BODACC où
-# l'entreprise a au moins un établissement actif dans le répertoire SIRENE sauf :
-#   - si l'unité légale n'a qu'un seul établissement
-#   - si aucun de ses établissements n'a été créé après la date de radiation
-RADIATIONS_PP_QUERY = """
+# Unités légales radiées au RCS d'après le BODACC mais toujours actives dans
+# SIRENE et/ou dans le RNE.
+#
+# Chaque ligne décrit l'état de l'unité légale dans les trois sources.
+# Les colonnes sont suffixées par leur source pour permettre de filtrer
+# les différents types d'incohérences. Exemples :
+#   - SIRENE actif, RNE actif, BODACC radié :
+#     `etat_administratif_sirene = 'A' and etat_administratif_rne = 'A'
+#     Potentiellement une liasse greffe perdue dans la nature avant 2023
+#   - SIRENE cessé, RNE actif, BODACC radié :
+#     `etat_administratif_sirene = 'C' and etat_administratif_rne = 'A'
+#     Potentiellement une radiation d’office pas intégrée par l’INPI à partir de 2025
+#
+# Une valeur état administratif à null indique l'absence de l'entreprise du répertoire
+# en question.
+
+RADIATIONS_INCOHERENCES_QUERY = """
+    WITH rne AS (
+        -- 179 SIREN sont en doublons dans la table immatriculation
+        -- Ce traitement peut être enlevé une fois ces SIREN dédoublonnés
+        -- Voir : https://github.com/annuaire-entreprises-data-gouv-fr/search-infra/issues/739
+        SELECT
+            siren,
+            MAX(date_immatriculation) AS date_immatriculation,
+            MAX(date_radiation) AS date_radiation,
+            MAX(date_fin_existence) AS date_fin_existence
+        FROM immatriculation
+        GROUP BY siren
+    )
     SELECT
         r.siren,
-        ul.nom_raison_sociale AS sirene_raison_sociale,
-        ul.nom AS sirene_nom,
-        ul.prenom AS sirene_prenom,
-        r.date AS bodacc_radiation_date,
-        r.date_publication AS bodacc_radiation_date_publication,
-        r.id_annonce AS bodacc_radiation_id_annonce,
-        'https://www.bodacc.fr/pages/annonces-commerciales-detail/?q.id=id:' || r.id_annonce AS bodacc_annonce_url
+        CASE
+            WHEN ul.nature_juridique_unite_legale = '1000'
+                OR ul.nature_juridique_unite_legale LIKE '2%'
+            THEN 'true'
+            ELSE 'false'
+        END AS est_personne_physique,
+        ul.etat_administratif_unite_legale AS etat_administratif_sirene,
+        CASE
+            WHEN rne.siren IS NULL THEN NULL
+            WHEN rne.date_radiation IS NULL THEN 'A'
+            ELSE 'C'
+        END AS etat_administratif_rne,
+        ul.nom_raison_sociale AS raison_sociale_sirene,
+        ul.nom AS nom_sirene,
+        ul.prenom AS prenom_sirene,
+        ul.nature_juridique_unite_legale AS nature_juridique_sirene,
+        ul.date_fermeture_unite_legale AS date_fermeture_sirene,
+        ul.en_sommeil AS en_sommeil_sirene,
+        rne.date_immatriculation AS date_immatriculation_rne,
+        rne.date_radiation AS date_radiation_rne,
+        rne.date_fin_existence AS date_fin_existence_rne,
+        r.date AS date_radiation_bodacc,
+        r.date_publication AS date_publication_bodacc,
+        r.id_annonce AS id_annonce_bodacc,
+        'https://www.bodacc.fr/pages/annonces-commerciales-detail/?q.id=id:'
+            || r.id_annonce AS url_annonce_bodacc,
+        r.visibility AS radiation_visible_annuaire
     FROM bodacc_radiations AS r
-    INNER JOIN unite_legale AS ul ON ul.siren = r.siren
+    LEFT JOIN unite_legale AS ul ON ul.siren = r.siren
+    LEFT JOIN rne ON rne.siren = r.siren
     WHERE r.est_radie = 1
       AND (
-        ul.nature_juridique_unite_legale = '1000'
-        OR ul.nature_juridique_unite_legale LIKE '2%'
-        )
-      and r.visibility_reason = 'ei_active_on_sirene'
-    ORDER BY r.date_publication DESC
-"""
-
-# Personnes morales radiées au RCS d'après le BODACC où
-# mais dont la radiation n'est pas reflétée dans SIRENE
-RADIATIONS_PM_QUERY = """
-    SELECT
-        r.siren,
-        ul.nom_raison_sociale AS sirene_raison_sociale,
-        ul.nature_juridique_unite_legale AS sirene_nature_juridique,
-        r.date AS bodacc_radiation_date,
-        r.date_publication AS bodacc_radiation_date_publication,
-        r.id_annonce AS bodacc_radiation_id_annonce,
-        'https://www.bodacc.fr/pages/annonces-commerciales-detail/?q.id=id:' || r.id_annonce AS bodacc_annonce_url
-    FROM bodacc_radiations AS r
-    INNER JOIN unite_legale AS ul ON ul.siren = r.siren
-    WHERE r.est_radie = 1
-        AND ul.nature_juridique_unite_legale != '1000'
-        AND ul.nature_juridique_unite_legale NOT LIKE '2%'
-        AND ul.etat_administratif_unite_legale = 'A'
-    ORDER BY r.siren
+        ul.etat_administratif_unite_legale = 'A'
+        OR (rne.siren IS NOT NULL AND rne.date_radiation IS NULL)
+      )
 """
